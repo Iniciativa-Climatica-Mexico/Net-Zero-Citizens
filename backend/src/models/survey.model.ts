20a0db627b41f12998ee0d9783e4fcfe76dde1ea
yna2131 <jn7924@gmail.com>
2023-09-05 17:34:07 -0600

    refactor(SurveyModel): Reestablish the standards of survey model in backend

--- backend/src/models/survey.model.ts
+++ backend/src/models/survey.model.ts
@@ -1,31 +1,49 @@
 import { db } from '../configs/database.config'
 import {
-    DataTypes,
-    Model,
-    InferAttributes,
-    InferCreationAttributes,
-    CreationOptional,
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
 } from 'sequelize'
 
-export interface Survey
-    extends Model<InferAttributes<Survey>, InferCreationAttributes<Survey>> {
-    id: CreationOptional<number>
-    title: string
-    description: string
-    startDate?: Date
-    endDate?: Date
-    createdAt?: Date
-    updatedAt?: Date
+export interface Surveys
+  extends Model<InferAttributes<Surveys>, InferCreationAttributes<Surveys>> {
+  surveyId: CreationOptional<number>
+  title: string
+  description: string
+  startDate?: Date
+  endDate?: Date
+  createdAt?: Date
+  updatedAt?: Date
 }
 
-export const SurveyModel = db.define<Survey>('Survey', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    startDate: DataTypes.DATE,
-    endDate: DataTypes.DATE,
+export const SurveysModel = db.define<Surveys>('SURVEYS', {
+  surveyId: {
+    type: DataTypes.INTEGER,
+    field: 'SURVEY_ID',
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING(225),
+    field: 'TITLE',
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING(500),
+    field: 'DESCRIPTION',
+    allowNull: true,
+  },
+  startDate: {
+    type: DataTypes.DATE,
+    field: 'START_DATE',
+    allowNull: false,
+  },
+  endDate: {
+    type: DataTypes.DATE,
+    field: 'END_DATE',
+    allowNull: false,
+  },
 })
