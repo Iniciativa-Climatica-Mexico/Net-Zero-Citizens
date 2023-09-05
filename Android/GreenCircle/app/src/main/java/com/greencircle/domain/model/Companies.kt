4ad3f5734305d89c240660b16de783f687ea9183
salgue441 <carlossalguero441@gmail.com>
2023-09-05 11:48:42 -0600

    feat(CompaniesModel): implement model for company entity

---
+++ Android/GreenCircle/app/src/main/java/com/greencircle/domain/model/Companies.kt
@@ -0,0 +1,40 @@
+/**
+ * @file Companies.kt
+ * @brief Modelo de datos de la tabla Companies
+ * @author Carlos Salguero
+ * @date 2023-09-05
+ * @version 0.0.1
+ *
+ * @copyright Copyright (c) - AGPL-3.0 License
+ *
+ */
+package com.greencircle.domain.model
+
+// Google Services
+import com.google.gson.annotations.SerializedName
+
+// Java Sql Imports
+import java.sql.Timestamp
+
+// Project files
+import com.greencircle.domain.model.Status
+
+/**
+ * @dataclass Companies
+ * @brief Modelo de datos de la tabla Companies
+ * @since 0.0.1
+ */
+data class Companies(
+    @SerializedName("company_id") val companyId: Int,
+    @SerializedName("user_id") val userId: Int,
+    @SerializedName("name") val name: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("location") val location: String,
+    @SerializedName("profile_picture") val profilePicture: String? = null,
+    @SerializedName("status") val status: Status,
+    @SerializedName("phone_number") val phoneNumber: String,
+    @SerializedName("web_page") val website: String? = null,
+    @SerializedName("created_at") val createdAt: Timestamp,
+    @SerializedName("updated_at") val updatedAt: Timestamp
+)
---
+++ Android/GreenCircle/app/src/main/java/com/greencircle/domain/model/Status.kt
@@ -0,0 +1,24 @@
+/**
+ * @file Status.kt
+ * @brief Enum de los estados de la empresa
+ * @author Carlos Salguero
+ * @date 2021-09-05
+ * @version 0.0.1
+ *
+ * @copyright Copyright (c) - AGPL-3.0 License
+ */
+package com.greencircle.domain.model
+
+// Google Services
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @enum Status
+ * @brief Enumerador de los estados de la empresa
+ * @since 0.0.1
+ */
+enum class Status {
+    @SerializedName("approved") APPROVED,
+    @SerializedName("pending") PENDING_APPROVAL,
+    @SerializedName("rejected") REJECTED
+}
