7aea37baad0e7a820c53e46168f8bad7aa7974e3
salgue441 <carlossalguero441@gmail.com>
2023-09-05 11:23:09 -0600

    chore(AndroidProject): added dependencies

--- Android/GreenCircle/app/build.gradle.kts
+++ Android/GreenCircle/app/build.gradle.kts
@@ -63,11 +63,32 @@ ktlint {
     }
 }
 
-dependencies {
+dependencies {  
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("jp.wasabeef:glide-transformations:4.3.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+
+    // Fragment
+    implementation("androidx.fragment:fragment-ktx:1.5.0")
+
+    // Activity
+    implementation("androidx.activity:activity-ktx:1.5.0")
+
+    // Databinding
+    implementation("androidx.databinding:databinding-runtime:7.1.2")
 }
