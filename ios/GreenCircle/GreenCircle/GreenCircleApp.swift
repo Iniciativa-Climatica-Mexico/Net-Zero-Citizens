d4c485dcf50ffee3b3c24de86ea02a3ae7858a41
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-09-04 23:17:59 -0600

    chore(ios): init xcode project

---
+++ ios/GreenCircle/GreenCircle/GreenCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenCircleApp.swift
+//  GreenCircle
+//
+//  Created by Ricardo Adolfo Fernández Alvarado on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GreenCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
