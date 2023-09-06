85c12902374549e787c2b6a32205dd586c82a027
DanielSebasCM <danielsebascm@hotmail.com>
2023-09-05 18:03:57 -0600

    fix(Dummies): Updated Dummies components

--- backend/src/services/dummy.service.ts
+++ backend/src/services/dummy.service.ts
@@ -1,8 +1,9 @@
-import { DummyModel, Dummy } from '../models/dummy.model'
+import { literal } from 'sequelize'
+import { DummiesModel, Dummy } from '../models/dummy.model'
 import { PaginatedQuery, PaginationParams } from '../utils/RequestResponse'
 
 export const getGreeting = async (name: string): Promise<string> => {
-  const dummy = await DummyModel.findOne({ where: { name } })
+  const dummy = await DummiesModel.findOne({ where: { name } })
   if (dummy) {
     console.log(dummy)
     return `Hello ${dummy.name} ${dummy.lastName}!`
@@ -12,11 +13,16 @@ export const getGreeting = async (name: string): Promise<string> => {
   }
 }
 
-export const getAllDummys = async <T>(
-  params: PaginationParams<T>
+export const getAllDummys = async (
+  params: PaginationParams<{ name: string }>
 ): Promise<PaginatedQuery<Dummy>> => {
-  return await DummyModel.findAndCountAll({
+  return await DummiesModel.findAndCountAll({
     limit: params.pageSize,
     offset: params.start,
+    // Case insensitive search
+    // where: literal(`LOWER(name) LIKE LOWER('%${params.name}%')`),
+    where: {
+      name: params.name,
+    },
   })
 }
