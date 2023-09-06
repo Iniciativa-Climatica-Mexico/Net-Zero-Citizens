0cff7b99d23f5316218bca4982bce4266c974b01
salgue441 <carlossalguero441@gmail.com>
2023-09-06 14:35:34 -0600

    chore(AndroidProject): hot fix for annotationProcessor for Glide. Change to kapt for allowing testing with Klint

--- Android/GreenCircle/app/build.gradle.kts
+++ Android/GreenCircle/app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     // Glide
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation ("jp.wasabeef:glide-transformations:4.3.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    kapt("com.github.bumptech.glide:compiler:4.12.0")
 
     // Gson
     implementation("com.google.code.gson:gson:2.8.9")
