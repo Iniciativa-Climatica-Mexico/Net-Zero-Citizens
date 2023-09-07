f917ce6b18bdc6ce39ff7b4d8fc632a9a8c4dae2
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-07 15:27:02 -0600

    chore(Auth): Add token verification for private routes

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -2,11 +2,14 @@ import DummyRouter from './dummy.routes'
 import UserRouter from './user.routes'
 import CompanyRouter from './company.routes'
 import AuthRouter from './auth.routes'
+import { validateToken } from '../middlewares/auth.middleware'
 import { Express } from 'express'
 
 export const initRouter = (app: Express) => {
   app.use('/auth', AuthRouter)
 
+  app.use(validateToken)
+  
   app.use('/dummy', DummyRouter)
   app.use('/company', CompanyRouter)
 }
