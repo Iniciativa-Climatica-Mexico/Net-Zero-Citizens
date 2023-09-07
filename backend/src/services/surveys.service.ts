e0504e9e96557cde076b02eea2027503e3f3b68d
yna2131 <jn7924@gmail.com>
2023-09-07 11:58:56 -0600

    feat(backend): Create getAllSurveys function in service.ts

--- backend/src/services/surveys.service.ts
+++ backend/src/services/surveys.service.ts
@@ -1,2 +1,19 @@
 import { SurveysModel, Surveys } from '../models/surveys.model'
 import { PaginatedQuery, PaginationParams } from '../utils/RequestResponse'
+
+/**
+ * @brief 
+ * Función del servicio que devuelve todas las encuestas de la base de datos
+ * @param params Los parámetros de paginación
+ * @returns Una promesa con las encuestas y la información de paginación
+ */
+export const getAllSurveys = async <T>(
+    params: PaginationParams<T>
+): Promise<PaginatedQuery<Surveys>> => {
+    return await SurveysModel.findAndCountAll({
+        limit: params.pageSize,
+        offset: params.start,
+    })
+}
+
+
