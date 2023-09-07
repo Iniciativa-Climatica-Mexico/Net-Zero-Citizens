c505f964eab9191eb5de5d3c9d02c34e9fc5fe19
Carlos <A01708634@tec.mx>
2023-09-07 10:46:53 -0600

    fix(adminPortal): Erase console logs

--- Admin/src/api/v1/company/index.ts
+++ Admin/src/api/v1/company/index.ts
@@ -4,7 +4,6 @@ const backendBaseUrl = 'http://localhost:3000';
 export const getPendingCompanies = async () => {
   try {
     const response = await axios.get(`${backendBaseUrl}/company/pending`);
-    console.log(response);
     return response.data;
   } catch (error) {
     console.error('Error fetching pending companies:', error);
