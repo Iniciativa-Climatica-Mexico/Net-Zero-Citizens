90d18f7b333f70d2d8173467378502e4500875b6
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-09-07 13:04:56 -0600

    style(Navbar): v2

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/MainActivity.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.greencircle.framework.views
 
-class MainActivity {
-    fun main() {
-        println("Hello, World!")
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.greencircle.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
