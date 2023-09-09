930d14cb9046023bae2387c30f9ba9fc2f30d866
Dani Gutiérrez <a01068056@tec.mx>
2023-09-08 23:18:09 -0600

    docs(ContactCompanies/iOS): Added documentation to files

--- ios/GreenCircle/GreenCircle/Data/Repository/CompanyRepository.swift
+++ ios/GreenCircle/GreenCircle/Data/Repository/CompanyRepository.swift
@@ -8,28 +8,38 @@
 import Foundation
 import Alamofire
 
-struct Api {
+/// Estructura con rutas para modelo de compañía
+struct ApiCompany {
     static let baseCompany = "http://localhost:3000/api/v1/company"
     struct Routes {
         static let company = "/company/"
     }
 }
 
+/// Protocolo que para cualquier clase que la use debe ser implementada
 protocol CompanyAPIProtocol {
     // GET all companies
     
+    /// Obtener compañía por UUID recibido desde el view listCompanies
+    ///  - Parameters: UUID
+    ///  - Returns: Modelo de Compañía
     func getCompanyById(companyId: UUID) async -> Company?
 }
 
+/// Implementación de repoitorio de Compañía con singleton
 class CompanyRepository: CompanyAPIProtocol {
+    /// Inicialización de servicio backEnd
     let backEndService: BackEndService
+    /// Inicialización de singleton de repositorio de compañía
     static let shared = CompanyRepository()
-    
+    /// Constructor que toma el valor del servicio del backEnd
     init(backEndService: BackEndService = BackEndService.shared) {
         self.backEndService = backEndService
     }
-    
+    /// Obtener compañía por UUID llamando al método del servicio del backend
+    ///   - Parameters: UUID de la compañía
+    ///   - Returns: Modelo de compañía
     func getCompanyById(companyId: UUID) async -> Company? {
-        return await backEndService.getCompanyById(url: URL(string: "\(Api.baseCompany)/\(companyId.uuidString.lowercased())")!)
+        return await backEndService.getCompanyById(url: URL(string: "\(ApiCompany.baseCompany)/\(companyId.uuidString.lowercased())")!)
     }
 }
--- ios/GreenCircle/GreenCircle/Presentation/ViewModels/CompanyViewModel.swift
+++ ios/GreenCircle/GreenCircle/Presentation/ViewModels/CompanyViewModel.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
+/// Implementación de view model de modelo de Compañía
 class CompanyViewModel: ObservableObject {
+    /// La compañía puede cambiar en la vista (se construye .onAppear())
     @Published var contentCompany: Company = Company(
             companyId: UUID(uuidString: "") ?? UUID(),
             userId: "",
@@ -35,11 +37,15 @@ class CompanyViewModel: ObservableObject {
                //  score: 0
             // )
         )
+    /// Requerimientos para hacer fetch de los datos de compañía
     var companyInfoRequirement: CompanyInfoRequirementProtocol
+    /// Para implementar el requerimiento en la vista que llame al ViewModel Compañía
     init(companyInfoRequirement: CompanyInfoRequirementProtocol = CompanyInfoRequirement.shared) {
         self.companyInfoRequirement = companyInfoRequirement
     }
+    /// Obtener información de la compañía mediante el requerimiento
     @MainActor
+    /// Actualización de la compañía si existe el UUID en base de datos
     func getCompanyById(idCompany: UUID) async {
         let resultCompany = await companyInfoRequirement.getCompanyById(id: idCompany)
         if let resultCompany = resultCompany {
