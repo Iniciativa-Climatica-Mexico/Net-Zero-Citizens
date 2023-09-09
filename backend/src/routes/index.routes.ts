1c3746bf06b843f370cb936eb56ef8a3b77fdb83
danfupo03 <danfupo@outlook.com>
2023-09-09 16:43:12 -0600

    feat(backendSurveys):Create router and added to index.routes

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,6 +1,7 @@
 import DummyRouter from './dummy.routes'
 import UserRouter from './user.routes'
 import CompanyRouter from './company.routes'
+import SurveyRouter from './survey.routes'
 import ReviewRouter from './review.routes'
 import { Express } from 'express'
 
@@ -8,5 +9,6 @@ export const initRouterV1 = (app: Express) => {
   app.use('/api/v1/dummy', DummyRouter)
   app.use('/api/v1/users', UserRouter)
   app.use('/api/v1/company', CompanyRouter)
+  app.use('/api/v1/survey', SurveyRouter)
   app.use('/api/v1/review', ReviewRouter)
 }
---
+++ backend/src/routes/survey.routes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import * as controller from '../controllers/survey.controller';
+
+const router = express.Router();
+
+router.get('/', controller.getAllSurveys);
+router.get('/:surveyId', controller.getSurveyById);
+
+//router.post('/', controller.createSurvey);
+
+export default router;
