7f7615f98a73149efe2de26a1961b354312fcdf6
Dani Gutiérrez <a01068056@tec.mx>
2023-09-08 23:18:09 -0600

    docs(ContactCompanies/iOS): Added documentation to files

--- ios/GreenCircle/GreenCircle/Data/Remote/BackEndService.swift
+++ ios/GreenCircle/GreenCircle/Data/Remote/BackEndService.swift
@@ -8,9 +8,18 @@
 import Foundation
 import Alamofire
 
+
+/// Inicializar clase de servicio con Singleton
+/// Se declaran los métodos para hacer las llamadas al backend
 class BackEndService {
+    /// Inicialización de singleton
     static let shared = BackEndService()
-    // GET all companies
+    // TODO GET all companies
+    
+    /// Obtener compañía por id
+    ///  - Parameters:
+    ///     - url: Backend url para obtener datos
+    ///  - Returns: Modelo de compañía o error en cualquier otro caso no válido
     func getCompanyById(url: URL) async -> Company? {
         let taskRequest = AF.request(url, method: .get).validate()
         let response = await taskRequest.serializingData().response
--- ios/GreenCircle/GreenCircle/Domain/UseCases/CompanyInfoRequirement.swift
+++ ios/GreenCircle/GreenCircle/Domain/UseCases/CompanyInfoRequirement.swift
@@ -7,17 +7,26 @@
 
 import Foundation
 
+/// Inicialización de protocolo de requerimientos
 protocol CompanyInfoRequirementProtocol {
     func getCompanyById(id: UUID) async -> Company?
 }
 
+/// Inicialización de clase de requerimientos de modelo de compañía
+/// No como se hará fetch de los datos, pero si definir los requerimientos
+/// para hacerlo
 class CompanyInfoRequirement: CompanyInfoRequirementProtocol {
+    /// Inicialización de repositorio para poder hacer el fetch de datos de compañía
     let companyDataRepository: CompanyRepository
-    static let shared = CompanyInfoRequirement() // Creacion de singleton
-    
+    /// Creación de Singleton
+    static let shared = CompanyInfoRequirement()
+    /// Constructor con recivimiento o por default
     init(companyDataRepository: CompanyRepository = CompanyRepository.shared) {
         self.companyDataRepository = companyDataRepository
     }
+    /// Definición de requerimiento para hacer el fetch
+    ///   - Parameters:UUID de compañía
+    ///   - Returns: Modelo de compañía
     func getCompanyById(id: UUID) async -> Company? {
         return await companyDataRepository.getCompanyById(companyId: id)
     }
