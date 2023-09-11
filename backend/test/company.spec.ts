fe119036a3301b3dc15348d6018c57afe90ea5ad
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-10 22:26:55 -0600

    test(Auth): Add test for company service methods

--- backend/test/company.spec.ts
+++ backend/test/company.spec.ts
@@ -1,7 +1,7 @@
 import chai from 'chai'
 import chaiExclude from 'chai-exclude'
 import { db, initDB } from '../src/configs/database.config'
-import { getAllCompanies } from '../src/services/company.service'
+import { getAllCompanies, createCompany, addProduct } from '../src/services/company.service'
 import { unwrap } from './utils'
 
 chai.use(chaiExclude)
@@ -51,7 +51,37 @@ const testData = [
     status: 'pending_approval',
   },
 ]
+
+const newCompany = {
+  companyId: 'comp-5678-efgh-0001',
+  name: 'Company 3',
+  description: 'Company 3 description',
+  email: 'example3@mail.com',
+  phoneNumber: '523456780',
+  webPage: null,
+  street: 'Company 2 street',
+  streetNumber: 2,
+  city: 'Company 3 city',
+  state: 'Company 3 state',
+  zipCode: 12346,
+  latitude: 0.111121,
+  longitude: 1.111211,
+  profilePicture: null,
+  pdfCurriculumUrl: 'https://picsum.photos/300',
+  pdfDicCdmxUrl: null,
+  pdfPeeFideUrl: null,
+  pdfGuaranteeSecurityUrl: 'https://picsum.photos/300',
+  status: 'pending_approval'
+}
+
+const companyProduct = {
+  companyId: 'comp-1234-efgh-0000',
+  productId: 'prod-1234-efgh-0000',
+  pdfProductCertificationUrl: 'https://picsum.photos/200',
+}
+
 const attributesToExclude = [
+  'companyProductId',
   'createdAt',
   'updatedAt',
 ]
@@ -70,5 +100,15 @@ describe('Company Service', () => {
     expect(unwrap(response).rows)
       .excluding(attributesToExclude)
       .to.deep.equal(testData)
+  })
+
+  it('should create a new company', async () => {
+    const res = await createCompany(newCompany)
+    expect(unwrap(res)).excluding(attributesToExclude).to.deep.equal(newCompany)
+  })
+
+  it('should add a product to a company', async () => {
+    const res = await addProduct(companyProduct)
+    expect(unwrap(res)).excluding(attributesToExclude).to.deep.equal(companyProduct)
   })
 })
