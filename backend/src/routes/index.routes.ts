0982f3c91acb29ebc68743d3a3bc51fff85c6e3b
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-10 19:49:06 -0600

    chore(Auth): create method and route for getting all products

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -2,6 +2,8 @@ import DummyRouter from './dummy.routes'
 import UserRouter from './user.routes'
 import CompanyRouter from './company.routes'
 import AuthRouter from './auth.routes'
+import ReviewRouter from './review.routes'
+import ProducRouter from './product.routes'
 import { validateToken, validateRole } from '../middlewares/auth.middleware'
 import { Express } from 'express'
 
@@ -10,7 +12,13 @@ export const initRouter = (app: Express) => {
 
   app.use(validateToken)
   
-  app.use('/dummy', DummyRouter)
-  app.use('/admin/dummy', validateRole(['admin']), DummyRouter)
-  app.use('/company', CompanyRouter)
+  // Dummy routes
+  app.use('/api/v1/admin/dummy', validateRole(['admin']), DummyRouter)
+
+  // Private Routes
+  app.use('/api/v1/dummy', DummyRouter)
+  app.use('/api/v1/users', UserRouter)
+  app.use('/api/v1/company', CompanyRouter)
+  app.use('/api/v1/review', ReviewRouter)
+  app.use('/api/v1/product', ProducRouter)
 }
