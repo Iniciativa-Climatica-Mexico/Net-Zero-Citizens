90da23be3f093dc3fa0c6cbec3fd1ee2ee9d059d
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-12 14:08:11 -0600

    chore(SplashScreen): Add splash screen

---
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/SplashscreenActivity.kt
@@ -0,0 +1,44 @@
+package com.greencircle.framework.views
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.greencircle.databinding.ActivitySplashscreenBinding
+import com.greencircle.framework.viewmodel.SplashscreenViewModel
+
+class SplashscreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashscreenBinding
+    private val viewModel: SplashscreenViewModel by viewModels()
+
+    override fun onCreate(savedInstances: Bundle?) {
+        super.onCreate(savedInstances)
+        initializeBinding()
+        viewModel.onCreate()
+        initializeObservers()
+    }
+
+    private fun initializeObservers() {
+        viewModel.finishedLoading.observe(
+            this,
+            Observer { finishedLoading ->
+                if (finishedLoading) {
+                    passViewGoToMain()
+                }
+            }
+        )
+    }
+
+    private fun initializeBinding() {
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun passViewGoToMain() {
+        var intent: Intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.greencircle.utils
+
+object Constants {
+    const val SPLASHSCREEN_DURATION = 3000L
+}
