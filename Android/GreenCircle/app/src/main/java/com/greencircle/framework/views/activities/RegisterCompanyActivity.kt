5fb85dff59497c7b00b18cff85c89fc430ce967e
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-12 17:23:04 -0600

    chore(Auth): RegisterCompany fragment, activity and viewmodel

---
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/activities/RegisterCompanyActivity.kt
@@ -0,0 +1,30 @@
+package com.greencircle.framework.views.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.greencircle.R
+import com.greencircle.databinding.ActivityRegisterCompanyBinding
+import com.greencircle.framework.views.fragments.RegisterCompanyFragment
+
+class RegisterCompanyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterCompanyBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRegisterCompanyBinding.inflate(
+            layoutInflater
+        )
+        setContentView(binding.root)
+
+        replaceFragment(RegisterCompanyFragment())
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        var fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.register_company_fragment, fragment)
+        fragmentTransaction.commit()
+    }
+}
