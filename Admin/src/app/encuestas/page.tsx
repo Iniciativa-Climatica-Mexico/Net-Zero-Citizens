eb684a552f774a1cc13d1ca03798c53a4cd174bd
yna2131 <jn7924@gmail.com>
2023-09-13 16:28:32 -0600

    feat(SurveyList): Create a button leading to create survey, and update on the list of surveys

--- Admin/src/app/encuestas/page.tsx
+++ Admin/src/app/encuestas/page.tsx
@@ -1,35 +1,57 @@
 import { Survey, fetchAllSurveys } from '@/api/survey'
+import Link from 'next/link'
 
 export default async function ListSurveys() {
   try {
     const response = await fetchAllSurveys()
     const surveysList = response.rows
     return (
-      <div className="text-primary flex-col flex items-center w-full">
-        <h1 className=" self-start font-extrabold my-8 mx-8 text-4xl text-zinc-800">Encuestas</h1>
-        <table className="table-fixed border-collapse w-[100rem]">
-          <thead className="">
-            <tr className="text-primary font-bold">
-              <th scope="col" className="py-8 border-b border-gray-700">
-                Title
-              </th>
-              <th scope="col" className="py-8 w-1/3 border-b border-gray-700">
-                Description
-              </th>
-              <th scope="col" className="py-8 border-b border-gray-700">
-                Start Date
-              </th>
-              <th scope="col" className="py-8 border-b border-gray-700">
-                End Date
-              </th>
-            </tr>
-          </thead>
-          {[
-            surveysList.map((survey, index) => {
-              return <SurveyComponent key={index} {...survey} />
-            }),
-          ]}
-        </table>
+      <div>
+        <div className="flex-row flex items-center justify-between my-8 mx-8">
+          <h1 className="self-start font-extrabold my-8 mx-8 text-4xl text-zinc-800">
+            Encuestas
+          </h1>
+          <Link href="/reportes/"><button className=" bg-primary hover:bg-primary-900 text-white font-bold py-2 px-4 mx-8 my-8 rounded self-end">
+            Crear Encuesta
+          </button></Link>
+        </div>
+        <div className="flex flex-col items-center w-full justify-center">
+          <table className="table-fixed border-collapse w-4/5">
+            <thead className="">
+              <tr className="text-primary font-bold">
+                <th
+                  scope="col"
+                  className="py-8 border-b w-[20%] border-gray-700"
+                >
+                  Title
+                </th>
+                <th
+                  scope="col"
+                  className="py-8 w-[40%] border-b border-gray-700"
+                >
+                  Description
+                </th>
+                <th
+                  scope="col"
+                  className="py-8 border-b border-gray-700 w-[20%]"
+                >
+                  Start Date
+                </th>
+                <th
+                  scope="col"
+                  className="py-8 border-b border-gray-700 w-[20%]"
+                >
+                  End Date
+                </th>
+              </tr>
+            </thead>
+            {[
+              surveysList.map((survey, index) => {
+                return <SurveyComponent key={index} {...survey} />
+              }),
+            ]}
+          </table>
+        </div>
       </div>
     )
   } catch (e: unknown) {
@@ -45,19 +67,27 @@ function SurveyComponent(props: Survey) {
   return (
     <tbody>
       <tr>
-        <td className="text-center truncate py-8 border-b border-gray-300 text-zinc-800"><a href="">{props.title}</a></td>
-        <td className="truncate border-b border-gray-300 text-zinc-800">{props.description}</td>
-        <td className="text-center border-b border-gray-300 text-zinc-800">{props.startDate.toString()}</td>
-        <td className="text-center border-b border-gray-300 text-zinc-800">
+        <td className="text-center truncate py-8 px-8 border-b border-gray-300 text-zinc-800">
+          <a href="">{props.title}</a>
+        </td>
+        <td className="truncate border-b py-8 px-8 border-gray-300 text-zinc-800">
+          {props.description}
+        </td>
+        <td className="text-center truncate border-b py-8 px-8 border-gray-300 text-zinc-800">
+          {props.startDate.toString()}
+        </td>
+        <td className="text-center border-b truncate py-8 px-8 border-gray-300 text-zinc-800">
           {props.endDate ? props.endDate.toString() : '---------'}
         </td>
       </tr>
     </tbody>
-    // <a className="text-txt bg-background" href="">
-    //   <h3 className="text-primary font-extrabold">{props.title}</h3>
-    //   <p className="pl-10">Descripción: {props.description}</p>
-    //   <p className="pl-10">Fecha de inicio: {props.startDate.toString()}</p>
-    //   <p className="pl-10">Fecha de cierre: {props.endDate?.toString()}</p>
-    // </a>
   )
+}
+
+function createSurvey() {
+
+}
+
+function viewDetailedSurvey() {
+
 }
