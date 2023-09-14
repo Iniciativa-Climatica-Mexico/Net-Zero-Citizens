4beef869b817287e5f3359f663f43a724f21427d
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-09-14 11:42:48 -0600

    style(Android): Add spacer

--- Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.greencircle.utils
 
 object Constants {
     // CAMBIEN ESTO AL DE SUS COMPUS PARA PROBAR
-    const val BASE_URL = "http://10.25.99.22:3000/api/v1/"
+    const val BASE_URL = "http://10.0.2.2:3000/api/v1/"
 }
