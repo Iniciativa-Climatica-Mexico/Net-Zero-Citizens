2f389ca559a2753279e7d77e1e1fd7185b658cfb
David Langarica <david.langarica@gmail.com>
2023-09-13 18:26:58 -0600

    style(reviewForm): Add view of review form

---
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/viewmodel/ReviewFormViewModel.kt
@@ -0,0 +1,7 @@
+package com.greencircle.framework.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ReviewFormViewModel : ViewModel() {
+    private var hello: String = "Hello World"
+}
---
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/fragments/ReviewFormFragment.kt
@@ -0,0 +1,35 @@
+package com.greencircle.framework.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.greencircle.databinding.FragmentReviewFormBinding
+import com.greencircle.framework.viewmodel.ReviewFormViewModel
+
+class ReviewFormFragment : Fragment() {
+    private var _binding: FragmentReviewFormBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: ReviewFormViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewModel = ViewModelProvider(this)[ReviewFormViewModel::class.java]
+
+        _binding = FragmentReviewFormBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
