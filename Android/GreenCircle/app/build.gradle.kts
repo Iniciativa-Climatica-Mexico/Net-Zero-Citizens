975f6c6ff59f684bd172e7890b557d815289dcf3
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-13 19:25:41 -0600

    chore(Auth): Get data from google for each user

--- Android/GreenCircle/app/build.gradle.kts
+++ Android/GreenCircle/app/build.gradle.kts
@@ -73,45 +73,34 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     // Google Login
-    implementation("com.google.android.gms:play-services-base:18.2.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
     // Glide
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("jp.wasabeef:glide-transformations:4.3.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
-
     // Gson
     implementation("com.google.code.gson:gson:2.8.9")
-
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-
     // Fragment
     implementation("androidx.fragment:fragment-ktx:1.6.1")
-
     // Activity
     implementation("androidx.activity:activity-ktx:1.7.2")
-
     // Data Binding
     implementation("androidx.databinding:databinding-runtime:8.1.1")
-
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-
     // Live Data
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-
     // Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
-
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 }
