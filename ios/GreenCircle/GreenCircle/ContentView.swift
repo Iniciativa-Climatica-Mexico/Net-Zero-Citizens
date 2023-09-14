127951b191d797d60d730cfa46e0dc9b3755d0e2
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-09-13 21:53:54 -0600

    feat(networkService): add ApiService class to make backend requests

--- ios/GreenCircle/GreenCircle/ContentView.swift
+++ ios/GreenCircle/GreenCircle/ContentView.swift
@@ -16,6 +16,11 @@ struct ContentView: View {
       Text("Hello, world!")
     }
     .padding()
+    .onAppear{
+      Task {
+        print(await DummyRepository.shared.getDummies()!)
+      }
+    }
   }
 }
 
---
+++ ios/GreenCircle/GreenCircle/Data/Remote/Network/NetworkAPIService.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkAPIService.swift
+//  GreenCircle
+//
+//  Created by Ricardo Adolfo Fernández Alvarado on 13/09/23.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkAPIService {
+  static let shared = NetworkAPIService()
+  static let decoder = JSONDecoder()
+  
+  init() {
+    NetworkAPIService.decoder.dateDecodingStrategy = .iso8601WithFractionalSeconds
+  }
+  
+  func getAllDummies(url: URL) async -> PaginatedQuery<Dummy>? {
+    let requestTask = AF.request(url, method: .get).validate()
+    let response = await requestTask.serializingData().response
+    
+    switch response.result {
+    case .success(let data):
+      do {
+        return try NetworkAPIService
+          .decoder
+          .decode(PaginatedQuery<Dummy>.self, from: data)
+      } catch {
+        debugPrint(error)
+        return nil
+      }
+    case let .failure(error):
+      debugPrint(error)
+      return nil
+    }
+  }
+}
