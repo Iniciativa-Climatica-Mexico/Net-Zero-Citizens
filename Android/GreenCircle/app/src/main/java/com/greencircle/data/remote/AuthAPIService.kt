021e6998209cbeb2c4092c1d594291a224a53fe0
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-14 09:22:51 -0600

    chore(Auth): Connect to backend

--- Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/AuthAPIService.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/AuthAPIService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.POST
 public interface AuthAPIService {
     // http:localhost:8000/api/v1/auth
 
-    data class GoogleLoginResponse(val message: String)
-    @POST("test")
+    data class GoogleLoginResponse(val message: String, val url: String)
+    @POST("/auth/test")
     suspend fun googleLogin(): GoogleLoginResponse
 }
--- Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.greencircle.utils
 
 object Constants {
     const val SPLASHSCREEN_DURATION = 3000L
-    const val SERVER_BASE_URL = "https://192.168.56.1:3000/api/v1/"
+    const val SERVER_BASE_URL = "http://10.25.87.135:3000/api/v1/"
 }
