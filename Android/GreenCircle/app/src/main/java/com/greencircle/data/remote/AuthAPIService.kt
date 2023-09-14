78cf5130191cdba607dff57ff39a08ea832d0739
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-14 09:28:36 -0600

    chore(Auth): Connected succesfully to backend

--- Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/AuthAPIService.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/AuthAPIService.kt
@@ -6,6 +6,6 @@ public interface AuthAPIService {
     // http:localhost:8000/api/v1/auth
 
     data class GoogleLoginResponse(val message: String, val url: String)
-    @POST("/auth/test")
+    @POST("auth/test")
     suspend fun googleLogin(): GoogleLoginResponse
 }
