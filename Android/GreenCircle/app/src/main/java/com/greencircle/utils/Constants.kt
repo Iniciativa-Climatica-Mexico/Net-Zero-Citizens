cffd6d06bc9d44b1f70a51e4b1fbce8df65c2950
Alejandra Cabrera <alejandraa.cruiz@gmail.com>
2023-09-14 13:47:08 -0600

    fix(Auth): Constants

--- Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.greencircle.utils
 
 object Constants {
     const val SPLASHSCREEN_DURATION = 3000L
-    const val SERVER_BASE_URL = "http://10.25.87.135:3000/api/v1/"
+    const val SERVER_BASE_URL = "http://10.25.126.240:3000/api/v1/"
 }
