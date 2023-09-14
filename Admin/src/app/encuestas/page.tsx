2bb09fd073684da662afbb285936dc6f5256f888
yna2131 <jn7924@gmail.com>
2023-09-13 20:07:04 -0600

    feat(SurveyList): Add more details to the button to see the detailed view of a survey

--- Admin/src/app/encuestas/page.tsx
+++ Admin/src/app/encuestas/page.tsx
@@ -8,12 +8,14 @@ export default async function ListSurveys() {
     return (
       <div>
         <div className="flex-row flex items-center justify-between my-8 mx-8">
-          <h1 className="self-start font-extrabold my-8 mx-8 text-4xl text-zinc-800">
+          <h1 className="self-start font-extrabold my-8 mx-8 text-4xl text-txt">
             Encuestas
           </h1>
-          <Link href="/reportes/"><button className=" bg-primary hover:bg-primary-900 text-white font-bold py-2 px-4 mx-8 my-8 rounded self-end">
-            Crear Encuesta
-          </button></Link>
+          <Link href="/reportes/">
+            <button className=" bg-primary hover:bg-primary-900 text-white font-bold py-2 px-4 mx-8 my-8 rounded self-end">
+              Crear Encuesta
+            </button>
+          </Link>
         </div>
         <div className="flex flex-col items-center w-full justify-center">
           <table className="table-fixed border-collapse w-4/5">
@@ -45,11 +47,13 @@ export default async function ListSurveys() {
                 </th>
               </tr>
             </thead>
-            {[
-              surveysList.map((survey, index) => {
-                return <SurveyComponent key={index} {...survey} />
-              }),
-            ]}
+            <tbody>
+              {[
+                surveysList.map((survey, index) => {
+                  return <SurveyComponent key={index} {...survey} />
+                }),
+              ]}
+            </tbody>
           </table>
         </div>
       </div>
@@ -65,29 +69,19 @@ export default async function ListSurveys() {
 
 function SurveyComponent(props: Survey) {
   return (
-    <tbody>
-      <tr>
-        <td className="text-center truncate py-8 px-8 border-b border-gray-300 text-zinc-800">
-          <a href="">{props.title}</a>
-        </td>
-        <td className="truncate border-b py-8 px-8 border-gray-300 text-zinc-800">
-          {props.description}
-        </td>
-        <td className="text-center truncate border-b py-8 px-8 border-gray-300 text-zinc-800">
-          {props.startDate.toString()}
-        </td>
-        <td className="text-center border-b truncate py-8 px-8 border-gray-300 text-zinc-800">
-          {props.endDate ? props.endDate.toString() : '---------'}
-        </td>
-      </tr>
-    </tbody>
+    <tr className="border-b border-gray-300">
+      <td className='truncate cursor-pointer  text-txt hover:text-primary hover:font-semibold'>
+        <a href={'/encuestas/' + props.surveyId} className="text-center py-8 px-8 ">{props.title}</a>
+      </td>
+      <td className="truncate py-8 px-8 border-gray-300 text-txt ">
+        {props.description}
+      </td>
+      <td className="text-center truncate py-8 px-8 border-gray-300 text-txt ">
+        {props.startDate.toString()}
+      </td>
+      <td className="text-center truncate py-8 px-8 border-gray-300 text-txt ">
+        {props.endDate ? props.endDate.toString() : '---------'}
+      </td>
+    </tr>
   )
-}
-
-function createSurvey() {
-
-}
-
-function viewDetailedSurvey() {
-
 }
