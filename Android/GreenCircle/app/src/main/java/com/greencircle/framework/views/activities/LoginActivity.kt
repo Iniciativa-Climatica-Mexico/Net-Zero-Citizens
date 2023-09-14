f7a1573228313dad74ffa1c5eeb3c90bfa8822da
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-13 19:25:41 -0600

    chore(Auth): Get data from google for each user

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/activities/LoginActivity.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/activities/LoginActivity.kt
@@ -3,24 +3,22 @@ package com.greencircle.framework.views.activities
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.greencircle.R
 import com.greencircle.databinding.ActivityLoginBinding
+import com.greencircle.framework.views.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-    private val REGISTER_COMPANY_REQUEST_CODE = 1
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        onRegisterCompany()
-    }
-
     private val registerCompanyActivityResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
@@ -30,15 +28,80 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-    private fun passViewGoToRegisterCompany() {
-        var intent: Intent = Intent(this, RegisterCompanyActivity::class.java)
-        registerCompanyActivityResult.launch(intent)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Methods
+        onRegisterCompany()
+
+        // Google Login
+        val googleButton = binding.root.findViewById<View>(R.id.sign_in_button)
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val acct: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
+        Log.d("GoogleSignIn", "acct: $acct")
+        if (acct != null) {
+            navigateToHome()
+        }
+
+        googleButton.setOnClickListener {
+            when (it.id) {
+                R.id.sign_in_button -> signIn(mGoogleSignInClient)
+            }
+        }
     }
+
+    private fun signIn(mGoogleSignInClient: GoogleSignInClient) {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        googleSignInActivityResult.launch(signInIntent)
+    }
+
+    private val googleSignInActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                Log.d("GoogleSignIn", "data: $data")
+                if (data != null && result.resultCode == Activity.RESULT_OK) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                    try {
+                        val account = task.getResult(ApiException::class.java)
+                        Log.d("GoogleSignIn", "Signed in successfully")
+                        Log.d("GoogleSignIn", "account: $account")
+                        navigateToHome()
+                    } catch (e: ApiException) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Something went wrong",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            } else if (result.resultCode == Activity.RESULT_CANCELED) {
+                // Handle the case where the user canceled the operation
+            }
+        }
 
     private fun onRegisterCompany() {
         val registerCompanyButton = binding.root.findViewById<View>(R.id.login_register_company)
         registerCompanyButton.setOnClickListener {
-            passViewGoToRegisterCompany()
+            navigateToRegisterCompany()
         }
+    }
+
+    private fun navigateToHome() {
+        var intent: Intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToRegisterCompany() {
+        var intent: Intent = Intent(this, RegisterCompanyActivity::class.java)
+        registerCompanyActivityResult.launch(intent)
     }
 }
