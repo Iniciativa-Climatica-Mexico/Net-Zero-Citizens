0c4ed0e9416086c2d49d9db3e678397ad250955e
Alejandra Cabrera <alejandraa.cruiz@gmail.com>
2023-09-16 11:29:07 -0600

    chore(Auth): Logic for setting TextViews in CreateUserFragment

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/fragments/RegisterUserFragment.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/fragments/RegisterUserFragment.kt
@@ -3,7 +3,6 @@ package com.greencircle.framework.views.fragments
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,22 +15,33 @@ import com.greencircle.databinding.FragmentRegisterUserBinding
 import com.greencircle.framework.views.activities.RegisterUserActivity
 import com.greencircle.utils.AuthUtils
 
+/**Constructor de "RegisterUserFragment"
+ *
+ * @constructor Incializa y crea la vista del "RegisterUserFragment". Navega a "CreateUserFragment".
+ */
 class RegisterUserFragment : Fragment() {
     private var _binding: FragmentRegisterUserBinding? = null
     private val binding get() = _binding!!
     private val authUtils = AuthUtils()
 
-    // Activity Result Contracts
+    /**
+     * Una propiedad para manejar el resultado de una operación de inicio de sesión de Google
+     * utilizando la API ActivityResult.
+     *
+     * Esta propiedad se inicializa con una lambda que procesa el resultado de la operación de inicio de sesión.
+     * Si la operación es exitosa, navega a un formulario y le pasa los argumentos de la autentificación.
+     * Si falla, muestra un mensaje emergente (toast).
+     */
     private val googleSignInActivityResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val data: Intent? = result.data
-                Log.d("GoogleSignIn", "data: $data")
                 if (data != null && result.resultCode == Activity.RESULT_OK) {
                     val task = GoogleSignIn.getSignedInAccountFromIntent(data)
                     try {
                         val account = task.getResult(ApiException::class.java)
-                        navigateToForm()
+                        val arguments = authUtils.getDataFromGoogleAccount(account)
+                        navigateToForm(arguments)
                     } catch (e: ApiException) {
                         Toast.makeText(
                             requireContext(), "Something went wrong", Toast.LENGTH_SHORT
@@ -43,6 +53,14 @@ class RegisterUserFragment : Fragment() {
             }
         }
 
+    /**
+     * Método que se llama cuando se crea la vista del fragmento de registro de usuario.
+     *
+     * @param inflater El inflador de diseño que se utiliza para inflar la vista.
+     * @param container El contenedor en el que se debe colocar la vista del fragmento.
+     * @param savedInstanceState La instancia de Bundle que contiene datos previamente guardados del fragmento.
+     * @return La vista inflada para el fragmento.
+     */
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -50,17 +68,23 @@ class RegisterUserFragment : Fragment() {
     ): View? {
         _binding = FragmentRegisterUserBinding.inflate(inflater, container, false)
 
-        // Google Login
         authUtils.googleLoginListener(binding, requireActivity(), googleSignInActivityResult)
 
-        // Inflate the layout for this fragment
         return binding.root
     }
 
-    // Navigate Methods
-    private fun navigateToForm() {
+    /**
+     * Navega hacia el fragmento "CreateUserFragment" dentro de la "RegisterUserActivity".
+     *
+     * Esta función se encarga de hacer la transición al fragmento "CreateUserFragment" desde el fragmento
+     * actual dentro de la "RegisterUserActivity". Opcionalmente, puede recibir un Bundle de argumentos
+     * que se pueden pasar al fragmento de destino.
+     *
+     * @param arguments Un Bundle opcional de argumentos que contiene la información de la cuenta de Google.
+     */
+    private fun navigateToForm(arguments: Bundle? = null) {
         val createUserFragment = CreateUserFragment()
         val activity = requireActivity() as RegisterUserActivity
-        activity.replaceFragment(createUserFragment)
+        activity.replaceFragment(createUserFragment,arguments)
     }
 }
