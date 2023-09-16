bb0d1a4eaf35b927fb44c13e71ca2d2b8a5d6865
Diego <sandoval.vargas.diego.1c@gmail.com>
2023-09-15 18:31:36 -0600

    chore(branch): merge with reviews

--- Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
@@ -3,9 +3,5 @@ package com.greencircle.utils
 object Constants {
     // CAMBIEN ESTO AL DE SUS COMPUS PARA PROBAR
     const val BASE_URL = "http://192.168.1.199:3000/api/v1/"
-}
-package com.greencircle.utils
-
-object Constants {
     const val CONTACTAR_PROVEEDOR = "CONTACTAR_PROVEEDOR"
 }
