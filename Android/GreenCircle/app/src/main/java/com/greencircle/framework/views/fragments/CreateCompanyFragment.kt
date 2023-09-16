5f1b875ec5919e74f772e46d58ee7ffab1a07074
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-16 15:11:14 -0600

    chore(Auth): Receive input values from regitser form

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/fragments/CreateCompanyFragment.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/fragments/CreateCompanyFragment.kt
@@ -9,7 +9,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.greencircle.R
 import com.greencircle.framework.viewmodel.CreateCompanyViewModel
 
@@ -59,14 +59,49 @@ class CreateCompanyFragment : Fragment() {
     private fun onSubmitListener(view: View) {
         val submitButton = view.findViewById<Button>(R.id.submit_create_company)
         submitButton.setOnClickListener {
-            Log.d("CreateCompanyFragment", "Send data to backend")
+            onSubmitHandler(view)
         }
     }
 
     private fun onSubmitHandler(view: View) {
-        val nameEditText: TextInputEditText = view.findViewById(R.id.companyNameTextField)
-        val descriptionEditText: TextInputEditText =
+        // Reference to all the fields
+        val nameInputLayout: TextInputLayout = view.findViewById(R.id.companyNameTextField)
+        val descriptionInputLayout: TextInputLayout =
             view.findViewById(R.id.companyDescriptionTextField)
+        val emailInputLayout: TextInputLayout = view.findViewById(R.id.companyEmailTextField)
+        val phoneInputLayout: TextInputLayout = view.findViewById(R.id.companyPhoneTextField)
+        val websiteInputLayout: TextInputLayout = view.findViewById(R.id.companyWebsiteTextField)
+        val streetInputLayout: TextInputLayout = view.findViewById(R.id.companyStreetTextField)
+        val streetNumberInputLayout: TextInputLayout =
+            view.findViewById(R.id.companyStreetNumberTextField)
+        val cityInputLayout: TextInputLayout = view.findViewById(R.id.companyCityTextField)
+        val stateInputLayout: TextInputLayout = view.findViewById(R.id.companyStateTextField)
+        val zipCodeInputLayout: TextInputLayout = view.findViewById(R.id.companyZipCodeTextField)
+
+        // Get the values
+        val name = nameInputLayout.editText?.text.toString()
+        val description = descriptionInputLayout.editText?.text.toString()
+        val email = emailInputLayout.editText?.text.toString()
+        val phone = phoneInputLayout.editText?.text.toString()
+        val website = websiteInputLayout.editText?.text.toString()
+        val street = streetInputLayout.editText?.text.toString()
+        val streetNumber = streetNumberInputLayout.editText?.text.toString()
+        val city = cityInputLayout.editText?.text.toString()
+        val state = stateInputLayout.editText?.text.toString()
+        val zipCode = zipCodeInputLayout.editText?.text.toString()
+
+        // Send the data to the backend
+        Log.d("CreateCompanyFragment", "Send data to backend")
+        Log.d("CreateCompanyFragment", "Name: $name")
+        Log.d("CreateCompanyFragment", "Description: $description")
+        Log.d("CreateCompanyFragment", "Email: $email")
+        Log.d("CreateCompanyFragment", "Phone: $phone")
+        Log.d("CreateCompanyFragment", "Website: $website")
+        Log.d("CreateCompanyFragment", "Street: $street")
+        Log.d("CreateCompanyFragment", "Street Number: $streetNumber")
+        Log.d("CreateCompanyFragment", "City: $city")
+        Log.d("CreateCompanyFragment", "State: $state")
+        Log.d("CreateCompanyFragment", "Zip Code: $zipCode")
     }
 
     private fun setTexts(arguments: Bundle, view: View) {
--- Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.greencircle.utils
 
 object Constants {
     const val SPLASHSCREEN_DURATION = 3000L
-    const val SERVER_BASE_URL = "http://192.168.1.87:3000/api/v1/"
+    const val SERVER_BASE_URL = "http://192.168.1.87:4000/api/v1/"
     const val GOOGLE_CLIENT_ID =
         "1012058349098-k1drm980aoroubmpskfsu9ra3shcucg7.apps.googleusercontent.com"
 }
