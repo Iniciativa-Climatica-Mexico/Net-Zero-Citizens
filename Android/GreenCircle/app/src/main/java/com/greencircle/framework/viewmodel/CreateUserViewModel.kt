9da23323afb18d19e091c1b211f14ea17588aaeb
Alejandra Cabrera <alejandraa.cruiz@gmail.com>
2023-09-16 16:05:21 -0600

    chore(Auth): template for Handler and fixes to documentation

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/viewmodel/CreateUserViewModel.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/viewmodel/CreateUserViewModel.kt
@@ -1,5 +1,7 @@
 package com.greencircle.framework.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.greencircle.data.remote.models.AuthResponse
@@ -7,12 +9,26 @@ import com.greencircle.domain.usecase.GoogleAuthRequirement
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+/**
+ * ViewModel para la creación de usuarios.
+ *
+ * Esta clase ViewModel se utiliza para gestionar las operaciones relacionadas con la creación de usuarios,
+ * incluyendo el inicio de sesión con Google.
+ */
 class CreateUserViewModel : ViewModel() {
     private val googleAuthRequirement = GoogleAuthRequirement()
+    private val _googleLoginResult = MutableLiveData<AuthResponse?>()
+    val googleLoginResult: LiveData<AuthResponse?> = _googleLoginResult
 
+    /**
+     * Realiza el inicio de sesión con Google utilizando el token proporcionado.
+     *
+     * @param token El token de autenticación de Google.
+     */
     fun googleLogin(token: String) {
         viewModelScope.launch(Dispatchers.IO) {
             val result: AuthResponse? = googleAuthRequirement(token)
+            _googleLoginResult.postValue(result)
         }
     }
 }
--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/activities/RegisterCompanyActivity.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/activities/RegisterCompanyActivity.kt
@@ -7,9 +7,20 @@ import com.greencircle.R
 import com.greencircle.databinding.ActivityRegisterCompanyBinding
 import com.greencircle.framework.views.fragments.RegisterCompanyFragment
 
+/**
+ * Actividad principal para el registro de empresas.
+ *
+ * Esta actividad permite a los usuarios registrarse en la aplicación. Se encarga de la gestión de fragmentos
+ * para mostrar las diferentes pantallas relacionadas con el proceso de registro.
+ */
 class RegisterCompanyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterCompanyBinding
 
+    /**
+     * Método llamado cuando se crea la actividad.
+     *
+     * @param savedInstanceState La instancia de Bundle que contiene datos previamente guardados de la actividad.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterCompanyBinding.inflate(
@@ -17,9 +28,16 @@ class RegisterCompanyActivity : AppCompatActivity() {
         )
         setContentView(binding.root)
 
+        // Reemplaza el fragmento principal con el fragmento de registro de empresa.
         replaceFragment(RegisterCompanyFragment())
     }
 
+    /**
+     * Reemplaza el fragmento actual con uno nuevo. En este caso "RegisterCompanyFragment"
+     *
+     * @param fragment El fragmento que se va a mostrar.
+     * @param data Un Bundle opcional de argumentos que contiene la información de la cuenta de Google.
+     */
     fun replaceFragment(fragment: Fragment, data: Bundle? = null) {
         fragment.arguments = data
         val fragmentManager = supportFragmentManager
@@ -29,11 +47,16 @@ class RegisterCompanyActivity : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 
+    /**
+     * Maneja el evento del botón "Atrás" en la barra de navegación.
+     */
     override fun onBackPressed() {
         val fragmentManager = supportFragmentManager
         if (fragmentManager.backStackEntryCount > 1) {
+            // Si hay fragmentos en la pila de retroceso, retrocede al fragmento anterior.
             fragmentManager.popBackStack()
         } else {
+            // Si no hay fragmentos en la pila de retroceso, finaliza la actividad.
             finish()
         }
     }
