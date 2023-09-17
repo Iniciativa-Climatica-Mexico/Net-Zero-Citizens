80085ebf619529bc8c9eed293f6c79b6516ec5ce
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-17 11:43:34 -0600

    refactor(Auth): remove logs

--- Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/CompanyAPIClient.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/CompanyAPIClient.kt
@@ -12,10 +12,8 @@ class CompanyAPIClient {
         authToken: String
     ): CompanyAPIService.CreateCompanyResponse? {
         api = CompanyNetworkModel(authToken)
-        Log.d("CreateCompany", "Fetching data from API")
         return try {
             val response = api.createCompany(company)
-            Log.d("CreateCompany", "Res: $response")
             response
         } catch (e: Exception) {
             e.printStackTrace()
