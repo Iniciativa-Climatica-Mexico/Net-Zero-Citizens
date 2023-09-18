36b94dc88df6695fc61bbb4bf8efe890e8680216
Santiago Rodriguez Murialdo <santiromur@gmail.com>
2023-09-17 21:19:31 -0600

    feat(Android): Add user option button

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/adapters/viewholders/UserReviewViewHolder.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/adapters/viewholders/UserReviewViewHolder.kt
@@ -1,13 +1,44 @@
 package com.greencircle.framework.adapters.viewholders
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
+import com.greencircle.R
 import com.greencircle.databinding.ItemUserReviewBinding
 import com.greencircle.domain.model.UserReview
 
 class UserReviewViewHolder(private var binding: ItemUserReviewBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
+    private val menuButton: ImageButton = binding.reviewCardOptionsButton
+
+    init {
+        menuButton.setOnClickListener {
+            val popupMenu = PopupMenu(itemView.context, menuButton)
+            popupMenu.inflate(R.menu.menu_user_review)
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.menu_item1 -> {
+                        // Handle menu item 1 click
+                        true
+                    }
+
+                    R.id.menu_item2 -> {
+                        // Handle menu item 2 click
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+            popupMenu.show()
+        }
+    }
     fun bind(item: UserReview, context: Context) {
         binding.reviewCardTitle.text = item.reviewTitle
         binding.reviewCardContent.text = item.review
