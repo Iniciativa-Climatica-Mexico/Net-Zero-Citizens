8f3a28a4e88fbf869e9a1a79947b45f260c9a9d5
Alejandra Cabrera <alejandraa.cruiz@gmail.com>
2023-09-18 14:34:49 -0600

    docs(Auth): Usecases documentation

--- Android/GreenCircle/app/src/main/java/com/greencircle/domain/usecase/GoogleAuthRequirement.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/domain/usecase/GoogleAuthRequirement.kt
@@ -3,8 +3,22 @@ package com.greencircle.domain.usecase
 import com.greencircle.data.remote.models.AuthResponse
 import com.greencircle.data.repository.AuthRepository
 
+/**
+ * Caso de uso para la autenticación con Google.
+ *
+ * Esta clase se utiliza para realizar la autenticación de un usuario utilizando un token de Google.
+ *
+ * @param repository El repositorio que proporciona métodos para interactuar con la autenticación.
+ */
 class GoogleAuthRequirement {
     private val repository = AuthRepository()
 
+    /**
+     * Realiza la autenticación con Google utilizando el token proporcionado.
+     *
+     * @param token El token de autenticación de Google.
+     * @return Un objeto [AuthResponse] que contiene la respuesta de la autenticación, o null
+     * si hay un error.
+     */
     suspend operator fun invoke(token: String): AuthResponse? = repository.googleLogin(token)
 }
