d4a10fe0e7c08a8aa706acfb518368061ad18848
Alejandra Cabrera <alejandraa.cruiz@gmail.com>
2023-09-18 14:49:01 -0600

    docs(Auth): Activities documentation

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/activities/SplashscreenActivity.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/activities/SplashscreenActivity.kt
@@ -8,10 +8,21 @@ import androidx.lifecycle.Observer
 import com.greencircle.databinding.ActivitySplashscreenBinding
 import com.greencircle.framework.viewmodel.SplashscreenViewModel
 
+/**
+ * Actividad de presentación que se muestra al iniciar la aplicación.
+ *
+ * Esta actividad se encarga de mostrar una pantalla de carga (splash screen) mientras se inicializa la aplicación.
+ * Una vez que la inicialización está completa, la actividad redirige al usuario a la pantalla de inicio de sesión.
+ */
 class SplashscreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashscreenBinding
     private val viewModel: SplashscreenViewModel by viewModels()
 
+    /**
+     * Método llamado cuando se crea la actividad.
+     *
+     * @param savedInstances El estado guardado de la actividad.
+     */
     override fun onCreate(savedInstances: Bundle?) {
         super.onCreate(savedInstances)
         initializeBinding()
@@ -19,6 +30,9 @@ class SplashscreenActivity : AppCompatActivity() {
         initializeObservers()
     }
 
+    /**
+     * Inicializa los observadores para las actualizaciones del estado de carga.
+     */
     private fun initializeObservers() {
         viewModel.finishedLoading.observe(
             this,
@@ -30,11 +44,17 @@ class SplashscreenActivity : AppCompatActivity() {
         )
     }
 
+    /**
+     * Inicializa el enlace de datos de la actividad.
+     */
     private fun initializeBinding() {
         binding = ActivitySplashscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
+     /**
+     * Redirige al usuario a la pantalla de inicio de sesión (LoginActivity).
+     */
     private fun passViewGoToMain() {
         var intent: Intent = Intent(this, LoginActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
