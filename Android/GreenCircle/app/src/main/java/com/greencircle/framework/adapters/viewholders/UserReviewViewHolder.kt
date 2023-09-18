0f8616ae918adb513b10a4cee1b873063eafd3be
unknown <a01709522@tec.mx>
2023-09-17 21:32:05 -0600

    fix(Android): fix klint check

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/adapters/viewholders/UserReviewViewHolder.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/adapters/viewholders/UserReviewViewHolder.kt
@@ -1,8 +1,6 @@
 package com.greencircle.framework.adapters.viewholders
 
 import android.content.Context
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
