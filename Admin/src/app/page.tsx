ed1fc6d345b69ba7aea40b6cf4c649956688a09b
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-18 00:33:21 -0600

    chore(Auth): Update Login UI

--- Admin/src/app/page.tsx
+++ Admin/src/app/page.tsx
@@ -1,24 +1,26 @@
 
-import Card from "@/components/Card" 
-import SignInButton from "@/components/SignInButton"
-import "bootstrap/dist/css/bootstrap.min.css" 
-import "../css/app.css"
-const background_image = new URL ("../imagenes/background.jpg", import.meta.url)
+'use client'
+import { useSession } from 'next-auth/react'
+import SignInButton from '@/components/SignInButton'
+import 'bootstrap/dist/css/bootstrap.min.css' 
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>
-          <div className="card">
-              <h4><b>GREEN CIRCLE</b></h4>
-               <p>Inicia sesión para continuar</p>  
-            <div className="container">
-                <SignInButton/>
-           </div>
-      
-          </div>
-        </div>
+  const {data: session} = useSession()
 
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <figure className="bg-neutral-50 shadow rounded-3xl p-10 w-96">
+        <header className='mb-5'>
+          <h1 className='txt-900 text-2xl font-semibold mb-3'>
+            {session ? `Bienvenido ${session?.user?.name}` : 'Inicia sesión con tu cuenta'}
+          </h1>
+          <p className='text-sm font-normal'>Nos da gusto verte de regreso</p>  
+        </header>
+        <div className='mt-5'>
+          <SignInButton/>
+        </div>
+      </figure>
+      <a href="#" className='text-gray-500'>Aviso de Privacidad</a>
     </main>
   )
 }
