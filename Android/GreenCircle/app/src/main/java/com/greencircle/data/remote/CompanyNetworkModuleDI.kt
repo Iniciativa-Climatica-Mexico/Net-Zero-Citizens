19d67af493d6b5f7ff2d5c2036fd0f60827e544b
Diego <sandoval.vargas.diego.1c@gmail.com>
2023-09-17 19:40:54 -0600

    fix(backend): se arreglan uuids incompatibles

--- Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/CompanyNetworkModuleDI.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/CompanyNetworkModuleDI.kt
@@ -1,18 +1,15 @@
 package com.greencircle.data.remote
 
 import com.greencircle.utils.Constants
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object CompanyNetworkModuleDI {
     private val gsonFactory = GsonConverterFactory.create()
-    private val okHttpClient = OkHttpClient()
 
     operator fun invoke(): CompanyAPIService {
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
-            .client(okHttpClient)
             .addConverterFactory(gsonFactory)
             .build()
             .create(CompanyAPIService::class.java)
