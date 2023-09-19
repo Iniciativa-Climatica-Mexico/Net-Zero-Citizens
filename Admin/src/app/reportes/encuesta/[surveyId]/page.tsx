f539790199c72a7e3f732fa5c838aacf11c8572e
Riosmena <emi.riosmena@gmail.com>
2023-09-19 12:48:32 -0600

    feat(frontSurvey): Add style to view

--- Admin/src/app/reportes/encuesta/[surveyId]/page.tsx
+++ Admin/src/app/reportes/encuesta/[surveyId]/page.tsx
@@ -23,12 +23,11 @@ export default async function SurveyReport(props: SurveyReportProps) {
             const data = question.answers?.map((answer) => answer.count)
             return (
               <div className="flex">
-                <div className="pl-20 w-1/2">
+                {/* Lado izquierdo de la ventana */}
+                <div className="pl-20 w-2/5 flex flex-col justify-between">
                   <QuestionComponent key={index} {...question} />
-                  <h3 className="text-black font-extrabold text-3xl pt-52 pb-6">
-                    Total de respuestas
-                  </h3>
-                  <table className="table-fixed border-collapse rounded-lg border border-slate-400">
+
+                  <table className="table-fixed border-collapse rounded-lg border border-slate-400 w-3/5">
                     <thead>
                       <tr>
                         <th className="px-10 py-4 border border-slate-400">
@@ -49,20 +48,53 @@ export default async function SurveyReport(props: SurveyReportProps) {
                     </tbody>
                   </table>
                 </div>
+
+                {/* Lado derecho de la ventana */}
                 <div className="pt-20 w-1/2">
                   <h2 className="text-black font-extrabold text-4xl">
                     Desglose de respuestas
                   </h2>
-                  <div className="w-2/3">
-                    {question.questionType != 'open' && (
-                      <ScaleChart
-                        {...{
-                          title: question.questionText,
-                          labels,
-                          data,
-                        }}
-                      />
-                    )}
+                  <div className="flex">
+                    {/* Tabla de labels */}
+                    <div className="pt-10 w-1/2">
+                      <table className="table-fixed border-collapse rounded-lg border border-slate-400">
+                        <thead>
+                          <tr>
+                            <th className="px-10 py-4 border border-slate-400">
+                              Opción
+                            </th>
+                            <th className="px-10 py-4 border border-slate-400">
+                              Total de respuestas
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {question.answers.map((ans) => (
+                            <tr>
+                              <td className="text-center p-4 border border-slate-400">
+                                {ans.label}
+                              </td>
+                              <td className="text-center p-4 border border-slate-400">
+                                {ans.count}
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+
+                    {/* Gráfica */}
+                    <div className="pl-4 w-1/2">
+                      {question.questionType != 'open' && (
+                        <ScaleChart
+                          {...{
+                            title: question.questionText,
+                            labels,
+                            data,
+                          }}
+                        />
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -89,7 +121,7 @@ const questionTypeMap = {
 function QuestionComponent(props: QuestionReport) {
   return (
     <div className="text-txt bg-background">
-      <h3 className="text-txt font-medium">{props.questionText}</h3>
+      <h3 className="text-txt font-medium text-xl">{props.questionText}</h3>
       <h3 className="text-black font-extrabold text-3xl pt-10 pb-3">
         Tipo de pregunta
       </h3>
