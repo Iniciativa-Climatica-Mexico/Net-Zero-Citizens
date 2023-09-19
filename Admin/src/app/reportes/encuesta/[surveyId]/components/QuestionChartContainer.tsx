296973b4df899c79ff0c30c983c304c6cebc7d86
Daniel Cajas <danielsebascm@hotmail.com>
2023-09-18 18:58:11 -0600

    feat(Questions): Added pagination for report

---
+++ Admin/src/app/reportes/encuesta/[surveyId]/components/QuestionChartContainer.tsx
@@ -0,0 +1,113 @@
+'use client'
+
+import {
+  fetchSurveyReport,
+  QuestionReport,
+  SurveyReport,
+  tabulatedAns,
+} from '@/api/report'
+
+import ScaleChart from './ScaleChart'
+import { useState } from 'react'
+
+export function QuestionChartContainer(surveyReport: SurveyReport) {
+  const [page, setPage] = useState(0)
+  const maxPage = Math.ceil(surveyReport.questions.length)
+
+  const onNextPage = () => setPage((page + 1) % maxPage)
+  const onPrevPage = () => setPage(page > 0 ? page - 1 : maxPage - 1)
+
+  const question = surveyReport.questions[page]
+  const labels = question.answers?.map((answer) => answer.label)
+  const data = question.answers?.map((answer) => answer.count)
+  return (
+    <div>
+      <h1 className="text-black text-5xl font-extrabold p-10">
+        {surveyReport.title}
+      </h1>
+      {(() => {
+        return (
+          <div className="flex">
+            <div className="pl-20 w-1/2">
+              <QuestionComponent {...question} />
+              <h3 className="text-black font-extrabold text-3xl pt-52 pb-6">
+                Total de respuestas
+              </h3>
+              <table className="table-fixed border-collapse rounded-lg border border-slate-400">
+                <thead>
+                  <tr>
+                    <th className="px-10 py-4 border border-slate-400">
+                      Número de respuestas
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="text-center p-4 border border-slate-400">
+                      <div className="bg-zinc-900 text-white rounded-full text-xl font-semibold p-2 px-4 inline-block">
+                        {question.answers
+                          .map((ans) => ans.count)
+                          .reduce((prev, curr) => (prev += curr))}
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="pt-20 w-1/2">
+              <h2 className="text-black font-extrabold text-4xl">
+                Desglose de respuestas
+              </h2>
+              <div className="w-2/3">
+                {question.questionType != 'open' && (
+                  <ScaleChart
+                    {...{
+                      title: question.questionText,
+                      labels: labels,
+                      data: data,
+                    }}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        )
+      })()}
+      <button onClick={onPrevPage}>Anterior</button>
+      <button onClick={onNextPage}>Siguiente</button>
+    </div>
+  )
+}
+
+const questionTypeMap = {
+  scale: 'Escala',
+  open: 'Abierta',
+  multiple_choice: 'Opción Múltiple',
+}
+
+function QuestionComponent(props: QuestionReport) {
+  return (
+    <div className="text-txt bg-background">
+      <h3 className="text-txt font-medium">{props.questionText}</h3>
+      <h3 className="text-black font-extrabold text-3xl pt-10 pb-3">
+        Tipo de pregunta
+      </h3>
+      <div className="bg-emerald-600 text-white font-semibold rounded-lg text-sm px-6 py-2 inline-block">
+        {questionTypeMap[props.questionType]}
+      </div>
+      {props.questionType == 'open' &&
+        props.answers?.map((answer, index) => {
+          return <AnswerComponent key={index} {...answer} />
+        })}
+    </div>
+  )
+}
+
+function AnswerComponent(props: tabulatedAns) {
+  return (
+    <div>
+      <h3>{props.label}</h3>
+      <p>{props.count}</p>
+    </div>
+  )
+}
--- Admin/src/app/reportes/encuesta/[surveyId]/page.tsx
+++ Admin/src/app/reportes/encuesta/[surveyId]/page.tsx
@@ -1,5 +1,5 @@
-import { fetchSurveyReport, QuestionReport, tabulatedAns } from '@/api/report'
-import ScaleChart from './components/ScaleChart'
+import { fetchSurveyReport } from '@/api/report'
+import { QuestionChartContainer } from './components/QuestionChartContainer'
 
 type SurveyReportProps = {
   params: {
@@ -7,70 +7,11 @@ type SurveyReportProps = {
   }
 }
 
-export default async function SurveyReport(props: SurveyReportProps) {
+export default async function SurveyReportComponent(props: SurveyReportProps) {
   try {
     const surveyId = props.params.surveyId
     const surveyReport = await fetchSurveyReport(surveyId)
-
-    return (
-      <div>
-        <h1 className="text-black text-5xl font-extrabold p-10">
-          {surveyReport.title}
-        </h1>
-        {[
-          surveyReport.questions.map((question, index) => {
-            const labels = question.answers?.map((answer) => answer.label)
-            const data = question.answers?.map((answer) => answer.count)
-            return (
-              <div className="flex">
-                <div className="pl-20 w-1/2">
-                  <QuestionComponent key={index} {...question} />
-                  <h3 className="text-black font-extrabold text-3xl pt-52 pb-6">
-                    Total de respuestas
-                  </h3>
-                  <table className="table-fixed border-collapse rounded-lg border border-slate-400">
-                    <thead>
-                      <tr>
-                        <th className="px-10 py-4 border border-slate-400">
-                          Número de respuestas
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td className="text-center p-4 border border-slate-400">
-                          <div className="bg-zinc-900 text-white rounded-full text-xl font-semibold p-2 px-4 inline-block">
-                            {question.answers
-                              .map((ans) => ans.count)
-                              .reduce((prev, curr) => (prev += curr))}
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <div className="pt-20 w-1/2">
-                  <h2 className="text-black font-extrabold text-4xl">
-                    Desglose de respuestas
-                  </h2>
-                  <div className="w-2/3">
-                    {question.questionType != 'open' && (
-                      <ScaleChart
-                        {...{
-                          title: question.questionText,
-                          labels,
-                          data,
-                        }}
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
-            )
-          }),
-        ]}
-      </div>
-    )
+    return <QuestionChartContainer {...surveyReport} />
   } catch (e: unknown) {
     if (e instanceof Error) {
       return <div>{e.message}</div>
@@ -78,37 +19,4 @@ export default async function SurveyReport(props: SurveyReportProps) {
       return <div>Unknown error</div>
     }
   }
-}
-
-const questionTypeMap = {
-  scale: 'Escala',
-  open: 'Abierta',
-  multiple_choice: 'Opción Múltiple',
-}
-
-function QuestionComponent(props: QuestionReport) {
-  return (
-    <div className="text-txt bg-background">
-      <h3 className="text-txt font-medium">{props.questionText}</h3>
-      <h3 className="text-black font-extrabold text-3xl pt-10 pb-3">
-        Tipo de pregunta
-      </h3>
-      <div className="bg-emerald-600 text-white font-semibold rounded-lg text-sm px-6 py-2 inline-block">
-        {questionTypeMap[props.questionType]}
-      </div>
-      {props.questionType == 'open' &&
-        props.answers?.map((answer, index) => {
-          return <AnswerComponent key={index} {...answer} />
-        })}
-    </div>
-  )
-}
-
-function AnswerComponent(props: tabulatedAns) {
-  return (
-    <div>
-      <h3>{props.label}</h3>
-      <p>{props.count}</p>
-    </div>
-  )
 }
