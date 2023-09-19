869e19411de547c64a034553bf760a9bd4fb5f6b
Diego <sandoval.vargas.diego.1c@gmail.com>
2023-09-18 19:48:43 -0600

    refactor(Android): fix linter styles

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/fragments/CompanyContactInfoFragment.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/fragments/CompanyContactInfoFragment.kt
@@ -1,13 +1,17 @@
 package com.greencircle.framework.views.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.greencircle.databinding.FragmentCompanyContactInfoBinding
 
+/**
+ * Fragmento que se encarga de mostrar la información de contacto de la empresa
+ * @constructor Crea un fragmento de company contact info
+ * @since 1.0.0
+ */
 class CompanyContactInfoFragment : Fragment() {
 
     private var _binding: FragmentCompanyContactInfoBinding? = null
