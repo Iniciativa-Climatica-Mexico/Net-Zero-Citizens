5c7090e1f30690d324d56a1196103ee45656deab
Dani Gutiérrez <a01068056@tec.mx>
2023-09-18 20:26:50 -0600

    fix(contactarProv): fixed company tests

--- backend/test/company.spec.ts
+++ backend/test/company.spec.ts
@@ -1,22 +1,16 @@
 import chai from 'chai'
 import chaiExclude from 'chai-exclude'
 import { db, initDB } from '../src/configs/database.config'
-import {
-  getAllCompanies,
-  getCompanyById,
-} from '../src/services/company.service'
-import { unwrap } from './utils'
+import { getCompanyById } from '../src/services/company.service'
 
 chai.use(chaiExclude)
 
 const { expect } = chai
-
-const testData = [  
+const testDataId = [
   {
     companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
     userId: '8de45630-2e76-4d97-98c2-9ec0d1f3a5b8',
     name: 'Company 1',
-    oneComment: 'This is a comment',
     description: 'Company 1 description',
     email: 'company@outlook.com',
     phone: '1234567890',
@@ -34,37 +28,44 @@ const testData = [
     pdfPeeFideUrl: 'https://www.company1.com/pdfPeeFide.pdf',
     pdfGuaranteeSecurityUrl:
       'https://www.company1.com/pdfGuaranteeSecurity.pdf',
-    pdfActaConstitutivaUrl:
-      'https://www.company1.com/pdfActaConstitutiva.pdf',
+    pdfActaConstitutivaUrl: 'https://www.company1.com/pdfActaConstitutiva.pdf',
     pdfIneUrl: 'https://www.company1.com/pdfIne.pdf',
     status: 'approved',
-    products: [{
-      productId: 'd1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
-      name: 'Product 1',
-      description:
+    products: [
+      {
+        productId: 'd1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
+        name: 'Product 1',
+        description:
           'Los paneles solares están compuestos por células solares, generalmente hechas de silicio, que tienen la capacidad de generar electricidad cuando son expuestas a la luz',
-      imageUrl: 'https://www.product1.com/image.png',
-      imageAltText: 'Product 1 Image',
-    },],
+        imageUrl: 'https://www.product1.com/image.png',
+        imageAltText: 'Product 1 Image',
+      },
+    ],
     rating: 4.3,
-    images: [{
-      companyImageId: '7f9a3d21-6b49-4e7c-ae56-1b0e8fcd9821',
-      companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
-      imageUrl: 'https://images.unsplash.com/photo-1613665813446-82a78c468a1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1758&q=80',
-      altText: 'Company 1 profile picture',
-    },
-    {
-      companyImageId: '7f9a3d21-6b49-4e7c-ae56-1b0e8fcd9822',
-      companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
-      imageUrl: 'https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1772&q=80',
-      altText: 'Company 1 Solar Panel',
-    },
-    {
-      companyImageId: '7f9a3d21-6b49-4e7c-ae56-1b0e8fcd9823',
-      companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
-      imageUrl: 'https://images.unsplash.com/photo-1595437193398-f24279553f4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80',
-      altText: 'Company 1 warehouse',
-    },]
+    oneComment: 'This is a comment',
+    images: [
+      {
+        companyImageId: '7f9a3d21-6b49-4e7c-ae56-1b0e8fcd9821',
+        companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
+        imageUrl:
+          'https://images.unsplash.com/photo-1613665813446-82a78c468a1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1758&q=80',
+        altText: 'Company 1 profile picture',
+      },
+      {
+        companyImageId: '7f9a3d21-6b49-4e7c-ae56-1b0e8fcd9822',
+        companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
+        imageUrl:
+          'https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1772&q=80',
+        altText: 'Company 1 Solar Panel',
+      },
+      {
+        companyImageId: '7f9a3d21-6b49-4e7c-ae56-1b0e8fcd9823',
+        companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
+        imageUrl:
+          'https://images.unsplash.com/photo-1595437193398-f24279553f4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80',
+        altText: 'Company 1 warehouse',
+      },
+    ],
   },
   {
     companyId: 'a2b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
@@ -91,24 +92,25 @@ const testData = [
     pdfIneUrl: 'https://www.company2.com/pdfIne.pdf',
     status: 'rejected',
     rating: 2.5,
-    products:[
+    products: [
       {
         productId: 'd2b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
         name: 'Product 2',
         description: 'Product 2 description',
-        imageUrl: 'https://images.adsttc.com/media/images/5ed5/4a01/b357/6538/ab00/048b/newsletter/aec-daily-solar-lighting_(2).jpg?1591036359',
+        imageUrl:
+          'https://images.adsttc.com/media/images/5ed5/4a01/b357/6538/ab00/048b/newsletter/aec-daily-solar-lighting_(2).jpg?1591036359',
         imageAltText: 'Product 2 Image',
       },
       {
         productId: 'd3b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
         name: 'Product 3',
         description: 'Product 3 description',
-        imageUrl: 'https://www.iberdrola.com/documents/20125/40918/Renovables_746x419.jpeg/171c88c6-834d-5309-0e0e-f758530dc3a9?t=1627967517593',
+        imageUrl:
+          'https://www.iberdrola.com/documents/20125/40918/Renovables_746x419.jpeg/171c88c6-834d-5309-0e0e-f758530dc3a9?t=1627967517593',
         imageAltText: 'Product 3 Image',
       },
     ],
-    images:[]
-
+    images: [],
   },
   {
     companyId: 'a2c0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
@@ -135,8 +137,8 @@ const testData = [
     pdfIneUrl: 'https://www.company3.com/pdfIne.pdf',
     status: 'pending_approval',
     rating: null,
-    products:[],
-    images:[]
+    products: [],
+    images: [],
   },
 ]
 
@@ -144,10 +146,6 @@ const attributesToExclude = [
   'createdAt',
   'updatedAt',
   'deletedAt',
-]
-
-const extraAttributes = [
-  'rating',
   'products',
   'images',
 ]
@@ -158,24 +156,6 @@ beforeEach(async () => {
 })
 
 describe('Company Service', () => {
-  it('should return all companies', async () => {
-    const response = await getAllCompanies({ start: 0, pageSize: 10 })
-
-    expect(unwrap(response))
-      .excludingEvery(attributesToExclude)
-      .excludingEvery(extraAttributes)
-      .to.deep.equal(testData)
-  })
-
-  it('should return all companies with pagination', async () => {
-    const response = await getAllCompanies({ start: 0, pageSize: 1 })
-
-    expect(unwrap(response))
-      .excludingEvery(attributesToExclude)
-      .excludingEvery(extraAttributes)
-      .to.deep.equal([testData[0]]) 
-  })
-
   it('should get company by id', async () => {
     const response = await getCompanyById(
       'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e'
@@ -183,14 +163,13 @@ describe('Company Service', () => {
 
     expect(response?.get())
       .excludingEvery(attributesToExclude)
-      .to.deep.equal(testData[0])
+      .to.deep.equal(testDataId[0])
   })
 
   it('should return null if company does not exist', async () => {
     const response = await getCompanyById(
       'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7f'
     )
-
     expect(response).to.be.null
   })
 })
