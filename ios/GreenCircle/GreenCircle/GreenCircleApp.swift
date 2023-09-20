3a50d8639d45e9d8e42e10d99abfff8b0429beb6
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-09-20 02:30:47 -0600

    feat(splashScreen): add splash screen to app

--- ios/GreenCircle/GreenCircle/GreenCircleApp.swift
+++ ios/GreenCircle/GreenCircle/GreenCircleApp.swift
@@ -10,18 +10,32 @@ import GoogleSignIn
 
 @main
 struct GreenCircleApp: App {
+  @State var isActive = false
+  
   var body: some Scene {
     WindowGroup {
-      CoordinatorView()
-        .environmentObject(UserData())
-        .onOpenURL { url in
-          GIDSignIn.sharedInstance.handle(url)
+      ZStack {
+        if isActive {
+          CoordinatorView()
+            .environmentObject(UserData())
+            .onOpenURL { url in
+              GIDSignIn.sharedInstance.handle(url)
+            }
+            .onAppear {
+              GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+                // Check if `user` exists; otherwise, do something with `error`
+              }
+            }
+        } else {
+          SplashScreenView()
         }
-        .onAppear {
-          GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
-            // Check if `user` exists; otherwise, do something with `error`
+      }.onAppear {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+          withAnimation {
+            self.isActive = true
           }
         }
+      }
     }
   }
 }
