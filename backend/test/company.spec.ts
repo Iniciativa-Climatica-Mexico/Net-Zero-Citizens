3edff5497d0345803308ae5c54c46b1036f594c8
sen <inarrituzanetti@outlook.com>
2023-09-20 11:46:54 -0600

    chore(bootstrap): se corrigieron pruebas de backend

--- backend/test/company.spec.ts
+++ backend/test/company.spec.ts
@@ -12,7 +12,7 @@ chai.use(chaiExclude)
 const { expect } = chai
 const testData = [
   {
-    companyId: 'comp-1234-efgh-0000',
+    companyId: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
     userId: 'abcd-1234-efgh-5679',
     name: 'Company 1',
     description: 'Company 1 description',
@@ -37,7 +37,7 @@ const testData = [
     status: 'pending_approval',
   },
   {
-    companyId: 'comp-1234-efgh-0001',
+    companyId: '3d6f0f67-40e5-4d58-8cb9-02b8d7a9d140',
     userId: 'abcd-1234-efgh-5680',
     name: 'Company 2',
     description: 'Company 2 description',
@@ -62,7 +62,7 @@ const testData = [
     status: 'pending_approval',
   },
   {
-    companyId: 'comp-1234-efgh-0002',
+    companyId: '5a8f29b5-98f0-41c4-a98f-0fb8ec22d7e7',
     userId: 'abcd-1234-efgh-5681',
     name: 'Company 3',
     description: 'Company 3 description',
@@ -87,7 +87,7 @@ const testData = [
     status: 'pending_approval',
   },
   {
-    companyId: 'comp-1234-efgh-0003',
+    companyId: '7e20e812-632e-4c25-8124-326f236db9bf',
     userId: 'abcd-1234-efgh-5682',
     name: 'Company 4',
     description: 'Company 4 description',
@@ -112,7 +112,7 @@ const testData = [
     status: 'pending_approval',
   },
   {
-    companyId: 'comp-1234-efgh-0004',
+    companyId: '2b5de26a-041d-4ea6-a144-3e36c0e28961',
     userId: 'abcd-1234-efgh-5683',
     name: 'Company 5',
     description: 'Company 5 description',
@@ -137,7 +137,7 @@ const testData = [
     status: 'pending_approval',
   },
   {
-    companyId: 'comp-1234-efgh-0005',
+    companyId: '6d5e7b2d-9c1e-4d4e-baa7-07b0b874401c',
     userId: 'abcd-1234-efgh-5684',
     name: 'Company 6',
     description: 'Company 6 description',
@@ -162,18 +162,18 @@ const testData = [
     status: 'pending_approval',
   },
   {
-    companyId: 'comp-1234-efgh-0006',
+    companyId: '8a604e88-74f3-4f4e-8b64-836bfa86b5d2',
     userId: 'abcd-1234-efgh-5685',
     name: 'Company 7',
     description: 'Company 7 description',
     email: 'example7@mail.com',
     phoneNumber: '123456795',
     webPage: 'www.company7.com',
-    street: '123 Main Street',
-    streetNumber: 456,
-    city: 'Cityville',
-    state: 'Stateville',
-    zipCode: 12345,
+    street: 'Lago de Chapala',
+    streetNumber: 167,
+    city: 'Queretaro',
+    state: 'Queretaro',
+    zipCode: 76230,
     profilePicture:
       'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR6XZP6rdW3VhHwZSxu3u4RCWyQFHTwl_4QSRK5t0km1FCytdaWwJEoNmW8c2ju5DRR4DbxppAQvVH441I',
     pdfCurriculumURL: 'https://example.com/company7-cv.pdf',
@@ -187,18 +187,18 @@ const testData = [
     status: 'pending_approval',
   },
   {
-    companyId: 'comp-1234-efgh-0007',
+    companyId: 'bc0f09b1-22b0-4a54-a9f0-5935c9590aeb',
     userId: 'abcd-1234-efgh-5686',
     name: 'Company 8',
     description: 'Company 8 description',
     email: 'example8@mail.com',
     phoneNumber: '123456796',
     webPage: 'www.company8.com',
-    street: '123 Main Street',
-    streetNumber: 456,
-    city: 'Cityville',
-    state: 'Stateville',
-    zipCode: 12345,
+    street: 'Blvd. villas del Meson',
+    streetNumber: 18,
+    city: 'Queretaro',
+    state: 'Queretaro',
+    zipCode: 76230,
     profilePicture:
       'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR6XZP6rdW3VhHwZSxu3u4RCWyQFHTwl_4QSRK5t0km1FCytdaWwJEoNmW8c2ju5DRR4DbxppAQvVH441I',
     pdfCurriculumURL: 'https://example.com/company8-cv.pdf',
@@ -212,18 +212,18 @@ const testData = [
     status: 'pending_approval',
   },
   {
-    companyId: 'comp-1234-efgh-0008',
+    companyId: 'e6ad188a-5baf-4b95-8c16-5a11d10a7ebc',
     userId: 'abcd-1234-efgh-5687',
     name: 'Company 9',
     description: 'Company 9 description',
     email: 'example9@mail.com',
     phoneNumber: '123456797',
     webPage: 'www.company9.com',
-    street: '123 Main Street',
-    streetNumber: 456,
-    city: 'Cityville',
-    state: 'Stateville',
-    zipCode: 12345,
+    street: 'Hacienda Grande',
+    streetNumber: 1405,
+    city: 'Queretaro',
+    state: 'Queretaro',
+    zipCode: 76180,
     profilePicture:
       'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR6XZP6rdW3VhHwZSxu3u4RCWyQFHTwl_4QSRK5t0km1FCytdaWwJEoNmW8c2ju5DRR4DbxppAQvVH441I',
     pdfCurriculumURL: 'https://example.com/company9-cv.pdf',
@@ -234,23 +234,23 @@ const testData = [
     pdfActaConstitutivaURL:
       'https://example.com/company9-acta-constitutiva.pdf',
     pdfINEURL: 'https://example.com/company9-ine.pdf',
-    status: 'pending_approval',
+    status: 'approved',
   },
   {
-    companyId: 'comp-1234-efgh-0009',
+    companyId: 'f17ff7a5-d808-43f9-af1a-127f6f0c1b4e',
     userId: 'abcd-1234-efgh-5688',
     name: 'Company 10',
     description: 'Company 10 description',
     email: 'example10@mail.com',
     phoneNumber: '123456798',
     webPage: 'www.company10.com',
-    street: '123 Main Street',
-    streetNumber: 456,
-    city: 'Cityville',
-    state: 'Stateville',
-    zipCode: 12345,
+    street: 'C. Monte Cárpatos',
+    streetNumber: 204,
+    city: 'Queretaro',
+    state: 'Queretaro',
+    zipCode: 76116,
     profilePicture:
-      'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR6XZP6rdW3VhHwZSxu3u4RCWyQFHTwl_4QSRK5t0km1FCytdaWwJEoNmW8c2ju5DRR4DbxppAQvVH441I',
+      'https://encryptesd-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcR6XZP6rdW3VhHwZSxu3u4RCWyQFHTwl_4QSRK5t0km1FCytdaWwJEoNmW8c2ju5DRR4DbxppAQvVH441I',
     pdfCurriculumURL: 'https://example.com/company10-cv.pdf',
     pdfDicCdmxURL: 'https://example.com/company10-dic-cdmx.pdf',
     pdfPeeFideURL: 'https://example.com/company10-pee-fide.pdf',
@@ -259,9 +259,10 @@ const testData = [
     pdfActaConstitutivaURL:
       'https://example.com/company10-acta-constitutiva.pdf',
     pdfINEURL: 'https://example.com/company10-ine.pdf',
-    status: 'pending_approval',
+    status: 'approved',
   },
 ]
+
 const attributesToExclude = [
   'companyId',
   'createdAt',
@@ -271,11 +272,11 @@ const attributesToExclude = [
 ]
 
 beforeEach(async () => {
+  await db.drop()
   await initDB()
 })
 
 afterEach(async () => {
-  await db.drop()
 })
 
 describe('Company Service', () => {
@@ -292,6 +293,6 @@ describe('Company Service', () => {
     const response = await getPendingCompanies({ start: 0, pageSize: 10 })
     expect(unwrap(response).rows)
       .excluding(attributesToExclude)
-      .to.deep.equal(testData)
+      .to.deep.equal(testData.filter((company) => company.status == 'pending_approval'))
   })
 })
