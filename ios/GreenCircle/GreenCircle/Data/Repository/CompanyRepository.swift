aee4ef7f86f1e58505c7a2e39e8f05f7bbffd2c5
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-09-19 22:31:30 -0600

    docs(auth): add documentation for Repositories

--- ios/GreenCircle/GreenCircle/Data/Repository/CompanyRepository.swift
+++ ios/GreenCircle/GreenCircle/Data/Repository/CompanyRepository.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// Clase representando la estructura de la API para las compañías
 class CompanyAPI {
   static let base = "http://localhost:3000/api/v1/company"
   struct Routes {
@@ -14,14 +15,20 @@ class CompanyAPI {
   }
 }
 
+/// Protocolo con las funciones del repositorio de Compañías
 protocol CompanyRepositoryProtocol {
   func postCompany(authToken: String, company: PostCompanyData) async
 }
 
+/// Clase con las funciones del repositorio de las compañías
 class CompanyRepository: CompanyRepositoryProtocol {
   static let shared = CompanyRepository()
   let nService: NetworkAPIService = NetworkAPIService()
   
+  /// Función que llama al servicio de conexión con la API para postear una  nueva compañía
+  /// - Parameters:
+  ///   - authToken: token de autenticación
+  ///   - company: el objeto con la información de la compañía
   func postCompany(authToken: String, company: PostCompanyData) async {
     await nService
       .postCompany(url:
--- ios/GreenCircle/GreenCircle/Data/Repository/UserRepository.swift
+++ ios/GreenCircle/GreenCircle/Data/Repository/UserRepository.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// Clase con la estructura de la API de autenticación
 class AuthAPI {
   static let base = "http://localhost:3000/api/v1/auth"
   struct Routes {
@@ -14,6 +15,7 @@ class AuthAPI {
   }
 }
 
+/// Clase con la estructura de la API de usuarios
 class UserAPI {
   static let base = "http://localhost:3000/api/v1/users"
   struct Routes {
@@ -21,15 +23,20 @@ class UserAPI {
   }
 }
 
+/// Protocolo con la declaración del repositorio del usuario
 protocol UserRepositoryProtocol {
   func postGoogleLogin(googleToken: String) async -> AuthResponse?
   func putUser(authToken: String, user: User) async
 }
 
+/// Clase con la funcionalidad del repositorio de usuario
 class UserRepository: UserRepositoryProtocol {
   let nService = NetworkAPIService.shared
   static let shared = UserRepository()
   
+  /// Llama al serivicio de API para postear un nuevo usuario por Google
+  /// - Parameter googleToken: token proporcionado por Google
+  /// - Returns: Una respuesta de autenticación, con Tokens e información del usuario
   func postGoogleLogin(googleToken: String) async -> AuthResponse? {
     return await nService
       .postGoogleSignIn(url: URL(
@@ -37,6 +44,10 @@ class UserRepository: UserRepositoryProtocol {
                         googleToken: googleToken)
   }
   
+  /// Actualiza la información de un usuario
+  /// - Parameters:
+  ///   - authToken: token de autenticación
+  ///   - user: información del usuario a actualizar
   func putUser(authToken: String, user: User) async {
     let url = URL(
       string: "\(UserAPI.base)\(UserAPI.Routes.userId)"
