ffe40cce5ed3428ba57a309d2723d86b69ed67fb
Carlos <A01708634@tec.mx>
2023-09-19 22:29:14 -0600

    fix(login): Successful connection with Google

--- Admin/src/app/layout.tsx
+++ Admin/src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar'
+import Providers from '@/components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Navbar />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
