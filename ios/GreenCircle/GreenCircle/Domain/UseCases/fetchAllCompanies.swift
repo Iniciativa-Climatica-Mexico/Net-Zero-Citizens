27d82b167cc018be25aac450392ce6b1e91cef8d
danfupo03 <danfupo@outlook.com>
2023-09-21 17:53:18 -0600

    chore(iOSProject): XCode currently working, missing merge changes

---
+++ ios/GreenCircle/GreenCircle/Domain/UseCases/fetchAllCompanies.swift
@@ -0,0 +1,20 @@
+//
+//  fetchAllCompanies.swift
+//  GreenCircle
+//
+//  Created by Dan FuPo on 21/09/23.
+//
+
+import SwiftUI
+
+struct fetchAllCompanies: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct fetchAllCompanies_Previews: PreviewProvider {
+    static var previews: some View {
+        fetchAllCompanies()
+    }
+}
