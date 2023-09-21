5f29fef5b72f491e530fb43e250319be80982ee0
salgue441 <carlossalguero441@gmail.com>
2023-09-20 19:22:31 -0600

    fix(Catalogue): Remove unused import

--- Android/GreenCircle/app/src/main/java/com/greencircle/data/repository/CatalogueRepository.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/data/repository/CatalogueRepository.kt
@@ -2,7 +2,6 @@ package com.greencircle.data.repository
 
 import com.greencircle.data.remote.CatalogueAPIClient
 import com.greencircle.domain.model.CompanySummary
-import java.util.UUID
 
 class CatalogueRepository {
     private var apiCatalogue = CatalogueAPIClient()
