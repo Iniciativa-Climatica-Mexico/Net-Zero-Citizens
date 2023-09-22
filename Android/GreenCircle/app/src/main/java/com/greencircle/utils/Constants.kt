831f851974f43c63e82fbd51c1aa3834e24fb98b
sen <inarrituzanetti@outlook.com>
2023-09-22 12:37:52 -0600

    chore(merge): merge de mapaListo

--- Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.greencircle.utils
 object Constants {
     const val CONTACTAR_PROVEEDOR = "CONTACTAR_PROVEEDOR"
     const val SPLASHSCREEN_DURATION = 3000L
-    const val SERVER_BASE_URL = "http://192.168.79.134:4000/api/v1/"
+    const val SERVER_BASE_URL = "http://10.25.87.77:4000/api/v1/"
     const val GOOGLE_CLIENT_ID =
         "1012058349098-k1drm980aoroubmpskfsu9ra3shcucg7.apps.googleusercontent.com"
 }
