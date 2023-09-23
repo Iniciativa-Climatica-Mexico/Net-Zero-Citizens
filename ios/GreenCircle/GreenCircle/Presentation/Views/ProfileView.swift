9b05da281d4e65cae7a866dca1dc8447bcfaef85
danfupo03 <danfupo@outlook.com>
2023-09-23 12:39:00 -0600

    chore(iOSFix): Delete repeated file and format vie

--- ios/GreenCircle/GreenCircle/Presentation/Views/ProfileView.swift
+++ ios/GreenCircle/GreenCircle/Presentation/Views/ProfileView.swift
@@ -1,29 +1,12 @@
-//
-//  ProfileView.swift
-//  GreenCircle
-//
-//  Created by ITESM on 07/09/23.
-//
-
 import SwiftUI
-
-
-
 
 struct ProfileView: View {
     @ObservedObject var modelUser: UserViewModel
 
-
-    
-    
     var body: some View {
-        
-        
-        
         ZStack {
             // Title Bar
             VStack {
-
                 TitleBarView(
                     title: "Mi Perfil",
                     leftIcon: nil,
@@ -36,23 +19,23 @@ struct ProfileView: View {
                 .navigationBarBackButtonHidden(true)
                 Spacer() // Esto empuja el TitleBarView hacia arriba
             }
-  //----------------------Perfil-------------------------------
+            
+            // Perfil
             VStack {
-                
-                //Imagen provicional
+                // Imagen provisional
                 Image("Sun")
-                .resizable() // Hacer que la imagen sea redimensionable
-                .frame(width: 100, height: 100)
+                    .resizable() // Hacer que la imagen sea redimensionable
+                    .frame(width: 100, height: 100)
                 
                 HStack {
-                    //Nombre del usuario
+                    // Nombre del usuario
                     Text(modelUser.contentUser.firstName)
                         .foregroundColor(Color.black)
                         .font(.system(size: 16))
                         .fontWeight(.semibold)
                         .padding(.top, 10)
                         .padding(.bottom, 2)
-                    //Apellido del Usuario
+                    // Apellido del Usuario
                     Text(modelUser.contentUser.lastName)
                         .foregroundColor(Color.black)
                         .font(.system(size: 16))
@@ -61,14 +44,14 @@ struct ProfileView: View {
                         .padding(.bottom, 2)
                 }
                 
-                //Bot{on cerrar sesión con navegaci{on provicional
+                // Botón cerrar sesión con navegación provisional
                 NavigationLink("Cerrar Sesión", destination: Example2View())
                     .foregroundColor(TitleBarColor.TitleBarColor)
                     .font(.system(size: 13))
                     .fontWeight(.bold)
                     .padding(.top, 4)
 
-//----------------------Botones------------------------------
+                // Botones
                 HStack {
                     Button(action: {
                         // Implementación futura
@@ -82,29 +65,27 @@ struct ProfileView: View {
                             .cornerRadius(8)
                     }
                     .padding(.trailing, 10)
+                    
                     Button(action: {
                     }) {
-
-                        NavigationLink(destination:  EditProfileView(modelUser: UserViewModel())) {
-                               Text("Editar perfil")
+                        NavigationLink(destination: EditProfileView(modelUser: UserViewModel())) {
+                            Text("Editar perfil")
                                 .foregroundColor(.white)
                                 .padding(.vertical, 12)
                                 .padding(.horizontal)
                                 .frame(maxWidth: .infinity)
                                 .background(TitleBarColor.TitleBarColor)
                                 .cornerRadius(8)
-                           }
+                        }
                     }
-                    .padding(.leading,10) // Añade padding para crear espacio entre los botones
-                    
-                    
+                    .padding(.leading, 10) // Añade padding para crear espacio entre los botones
                 }
                 .padding(.horizontal, 40) // Añade padding horizontal para que los botones no lleguen hasta el borde de la vista
                 .padding(.top, 24) // Reduciendo el padding top para acercar los botones
 
                 Spacer()
                 
-//--------------------Seccón de Reseñas-----------------------------------------
+                // Sección de Reseñas
                 Text("Reseñas Escritas (0)")
                     .font(.system(size: 20))
                     .fontWeight(.bold)
@@ -112,8 +93,9 @@ struct ProfileView: View {
                     .padding(.leading)
                     .foregroundColor(.black)
                     .frame(maxWidth: .infinity, alignment: .leading)
-                ScrollView{
-                    //Aquí irán las tarjetas de reseñas
+                
+                ScrollView {
+                    // Aquí irán las tarjetas de reseñas
                 }
             }
             .padding(.top, 70)
@@ -122,32 +104,12 @@ struct ProfileView: View {
                     await modelUser.fetchUserById(idUser: "8de45630-2e76-4d97-98c2-9ec0d1f3a5b8")
                 }
             }
-            
-            
         }
-        
-        
-      
-
-        .onAppear {
-            Task {
-                // Nueva solicitud para obtener los datos actualizados del usuario
-                do {
-                    await modelUser.fetchUserById(idUser: "8de45630-2e76-4d97-98c2-9ec0d1f3a5b8")
-                }
-            }
-        }
-       
-        
-        
-        
     }
 
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView(modelUser: UserViewModel())
-
+    struct ProfileView_Previews: PreviewProvider {
+        static var previews: some View {
+            ProfileView(modelUser: UserViewModel())
+        }
     }
-}
-
 }
