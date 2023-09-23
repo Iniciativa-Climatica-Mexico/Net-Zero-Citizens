d52af620e1ca60b076ed437644880261136eca7d
Dani Gutiérrez <a01068056@tec.mx>
2023-09-23 15:46:10 -0600

    fix(iOS): Fixes to integration

--- ios/GreenCircle/GreenCircle/Presentation/Views/Company/CatalogView.swift
+++ ios/GreenCircle/GreenCircle/Presentation/Views/Company/CatalogView.swift
@@ -10,34 +10,61 @@ import SwiftUI
 
 struct CatalogView: View {
   @StateObject var viewModel = CompanyViewModel()
+  @State var availableProducts = false
   var body: some View {
     
     NavigationStack {
       List(viewModel.companies) { company in
-        NavigationLink(destination: ContactCompanyView(idCompany: company.companyId)) {
-          ZStack{
+          ZStack {
+            NavigationLink(destination: ContactCompanyView(idCompany: company.companyId)) {
+              EmptyView()
+            }.buttonStyle(PlainButtonStyle())
             RoundedRectangle(cornerRadius: 25, style: .continuous)
               .fill(.white)
               .shadow(color: .gray, radius: 2)
-            HStack{
-              Image(systemName: "square.fill")
-                .resizable()
-                .frame(width: 90, height: 90.0)
-                .foregroundColor(.gray)
-              
+              .frame(width: 350, height: 160) // Adjusted card size
+
+            HStack {
+              if let imageURL = URL(string: company.images?.first?.imageUrl ?? "") {
+                AsyncImage(url: imageURL) { phase in
+                  switch phase {
+                    case .empty:
+                      Image(systemName: "square.fill")
+                        .resizable()
+                        .frame(width: 90, height: 100)
+                        .foregroundColor(.gray)
+                    case .success(let image):
+                      image
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(10, corners: [.bottomLeft, .bottomRight, .topLeft, .topRight])
+                        .frame(width: 100, height: 100)
+                    case .failure:
+                      Text("Failed to load Image!!")
+                    @unknown default:
+                      fatalError()
+                  }
+                }
+              } else {
+                Image(systemName: "square.fill")
+                  .resizable()
+                  .frame(width: 90, height: 90)
+                  .foregroundColor(.gray)
+              }
+
               VStack(spacing: 10) {
                 Text(company.name)
                   .font(.title)
                 
                   .fontWeight(.bold)
                   .foregroundColor(.black)
-                HStack{
-                  Image(systemName: "location.fill")
-                    .foregroundColor(.green)
-                  Text("\(company.streetNumber) \(company.street)")
-                    .foregroundColor(.green)
+                HStack {
+                    Image(systemName: "location.fill")
+                      .foregroundColor(.green)
+                    Text("\(company.streetNumber) \(company.street)")
+                      .foregroundColor(.green)
                 }
-                HStack{
+                HStack {
                   Image(systemName: "star.fill")
                     .foregroundColor(.green)
                   Image(systemName: "star.fill")
@@ -57,25 +84,23 @@ struct CatalogView: View {
                 .foregroundColor(.gray)
               
             }
-            
-            
-            .frame(width: 380, height: 180)
+            .frame(width: 330, height: 180)
           }
+
           .listRowSeparator(.hidden)
           .listRowInsets(.init(top: 10, leading:10, bottom:0, trailing:10))
           .padding(.vertical, 5)
         }
-        
+      .padding(EdgeInsets(top: 0, leading: 2, bottom: 0, trailing: 2))
+
         .listRowInsets(.init(top: 10, leading:10, bottom:0, trailing:10))
         
         .listStyle(.plain)
         .navigationTitle("Proveedores")
-        
-        
-      }
+        .navigationBarTitleDisplayMode(.inline)
+
     }
-    .onAppear {
-      
+      .onAppear {
       Task {
         await viewModel.fetchAllCompanies()
       }
