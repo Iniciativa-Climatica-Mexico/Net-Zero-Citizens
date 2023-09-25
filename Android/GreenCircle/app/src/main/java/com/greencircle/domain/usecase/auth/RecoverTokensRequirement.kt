dbdd1d1a2023c56056f188db5e0bee4349fd4c8d
Sergio Garnica <sgarnica1902@gmail.com>
2023-09-24 23:55:50 -0600

    fix(Linter): fix linter

--- Android/GreenCircle/app/src/main/java/com/greencircle/domain/usecase/auth/RecoverTokensRequirement.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/domain/usecase/auth/RecoverTokensRequirement.kt
@@ -6,9 +6,7 @@ import com.greencircle.domain.model.auth.Tokens
 
 /**
  * Caso de uso para recuperar tokens de autenticación y actualización almacenados.
- *
  * Esta clase se utiliza para recuperar los tokens de autenticación y actualización almacenados en el almacenamiento compartido.
- *
  * @param context El contexto de la aplicación Android necesario para acceder al almacenamiento compartido.
  */
 class RecoverTokensRequirement(private val context: Context) {
@@ -18,7 +16,6 @@ class RecoverTokensRequirement(private val context: Context) {
 
     /**
      * Recupera los tokens de autenticación y actualización almacenados.
-     *
      * @return Un objeto [Tokens] que contiene los tokens recuperados.
      * @throws Exception Si los tokens no se encuentran en el almacenamiento compartido.
      */
