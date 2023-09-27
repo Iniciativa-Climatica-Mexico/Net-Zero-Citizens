899fece77f6b11543c6cf227d6793c0bb5285853
unknown <a01709522@tec.mx>
2023-09-27 00:39:48 -0600

    feat(backend): Add getAllComplaints and getComplaintById features

--- backend/src/controllers/complaint.controller.ts
+++ backend/src/controllers/complaint.controller.ts
@@ -1,14 +1,14 @@
 import Complaint from '../models/complaint.model'
 import * as ComplaintService from '../services/complaints.service'
-import { NoRecord, Paginator, PaginationParams } from '../utils/RequestResponse'
+import { NoRecord, Paginator, PaginationParams, PaginatedQuery } from '../utils/RequestResponse'
 import { RequestHandler } from 'express'
 
 /**
  * @brief
- * Función del controlador que devuelve todos los reviews
+ * Función del controlador que devuelve todos los complaints
  * de la base de datos
  * @param req La request HTTP al servidor
- * @param res Un objeto paginador con los reviews y la
+ * @param res Un objeto paginador con los complaints y la
  *            información de paginación
  */
 
@@ -26,42 +26,53 @@ export const getAllComplaints: RequestHandler<
     },
   }
   try{
-  const complaints = await ComplaintService.getAllComplaints(params)
-  res.json({
-    rows: complaints.rows,
-    start: params.start,
-    pageSize: params.pageSize,
-    total: complaints.count,
-  })
-}
-catch(error){
-  res.status(400).json({error: 'Error getting complaints'})
-}
-}
-
-export const getComplaintById: RequestHandler<
-  NoRecord,
-  Complaint | { message: string },
-  NoRecord,
-  { id: string }
-> = async (req, res) => {
-  try {
-    const complaint = await ComplaintService.getComplaintById(req.params.id)
-
-    if (!complaint) {
-      res.status(404).json({ message: 'Complaint not found' })
-    } else {
-      res.json(complaint)
-    }
-  } catch (error) {
-    res.status(500).json({ message: 'Internal server error' })
+    const complaints = await ComplaintService.getAllComplaints(params)
+    res.json({
+      rows: complaints.rows,
+      start: params.start,
+      pageSize: params.pageSize,
+      total: complaints.count,
+    })
+  }
+  catch(error){
+    res.status(400).json({error: 'Error getting complaints'})
   }
 }
+
+/**
+ * @brief
+ * Función del controlador que devuelve un complaint por id de la base de datos
+ * @param req La request HTTP al servidor
+ * @param res Un objeto paginador con el complaint y la
+ *           información de paginación
+ */
+
+export const getComplaintById: RequestHandler<
+  { complaintId: string },
+  Paginator<Complaint>,
+  NoRecord,
+  NoRecord
+> = async (req, res) => {
+  const { complaintId } = req.params
+  const params = {
+    start: req.query.start || 0,
+    pageSize: req.query.pageSize || 10,
+    complaintId: complaintId,
+  }
+  const review = await ComplaintService.getComplaintById(params)
+  res.json({
+    rows: review.rows,
+    start: params.start,
+    pageSize: params.pageSize,
+    total: review.count,
+  })
+}
+
 
 
 export const getComplaintByCompany: RequestHandler<
   NoRecord,
-  Complaint | { message: string },
+  PaginatedQuery<Complaint> | { message: string },
   NoRecord,
   PaginationParams<{ companyId: string }>
 > = async (req, res) => {
@@ -84,7 +95,7 @@ export const getComplaintByCompany: RequestHandler<
 
 export const getComplaintByUser: RequestHandler<
   NoRecord,
-  Complaint | { message: string },
+  PaginatedQuery<Complaint> | { message: string },
   NoRecord,
   PaginationParams<{ userId: string }>
 > = async (req, res) => {
@@ -98,7 +109,7 @@ export const getComplaintByUser: RequestHandler<
     if (!complaint) {
       res.status(404).json({ message: 'Complaint not found' })
     } else {
-      res.json()
+      res.json(complaint)
     }
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' })
