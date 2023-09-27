e6bcf89be76447b8cf263fb226c50836cb8b539f
unknown <a01709522@tec.mx>
2023-09-27 00:38:32 -0600

    feat(backend): Add complaint objects to database

--- backend/src/bootstrap/complaint.bootstrap.ts
+++ backend/src/bootstrap/complaint.bootstrap.ts
@@ -1,34 +1,39 @@
-import { Bootstrapper } from './Bootstraper'
 import Complaint from '../models/complaint.model'
-
+import { Bootstrapper } from './Bootstraper'
 export default class ComplaintBootstrap extends Bootstrapper {
   async run() {
     Complaint.bulkCreate([
       {
-        complaintId: 'comp-1234-efgh-0000',
-        userId: 'user-1234-efgh-0000',
-        companyId: 'comp-1234-efgh-0009',
+        complaintId: 'compl-1234-efgh-0000',
+        userId: '8de45630-2e76-4d97-98c2-9ec0d1f3a5b8',
+        companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
         complaintSubject: 'Productos Defectuosos',
+        complaintDescription: 'El vendedor me insultó',
+        complaintStatus: 'active'
       },
       {
-        complaintId: 'comp-1235-efgh-0000',
-        userId: 'user-1234-efgh-0000',
-        companyId: 'comp-1234-efgh-0009',
+        complaintId: 'compl-1235-efgh-0000',
+        userId: '8de45630-2e76-4d97-98c2-9ec0d1f3a5b8',
+        companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
         complaintSubject: 'Inconformidad con el producto / servicio',
+        complaintDescription: 'El vendedor me insultó',
+        complaintStatus: 'active'
       },
       {
-        complaintId: 'comp-1236-efgh-0000',
-        userId: 'user-1234-efgh-0000',
-        companyId: 'comp-1234-efgh-0009',
+        complaintId: 'compl-1236-efgh-0000',
+        userId: '8de45630-2e76-4d97-98c2-9ec0d1f3a5b8',
+        companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
         complaintSubject: 'Comportamiento Inapropiado',
         complaintDescription: 'El vendedor me insultó',
+        complaintStatus: 'active'
       },
       {
-        complaintId: 'comp-1237-efgh-0000',
-        userId: 'user-1234-efgh-0000',
-        companyId: 'comp-1234-efgh-0009',
+        complaintId: 'compl-1237-efgh-0000',
+        userId: '8de45630-2e76-4d97-98c2-9ec0d1f3a5b8',
+        companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
         complaintSubject: 'Mal Servicio',
         complaintDescription: 'aaaaaaaaaaaaaaaaaaaaaaaaaa',
+        complaintStatus: 'active'
       }
     ])
   }
