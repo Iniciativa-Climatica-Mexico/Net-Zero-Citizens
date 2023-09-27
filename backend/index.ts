60b0ae4df60b085491905c353e25fab7c908da51
santiagordz <santiromur@gmail.com>
2023-09-27 14:05:42 -0600

    chore(Merge): Pull from dev

--- backend/index.ts
+++ backend/index.ts
@@ -18,8 +18,6 @@ app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
 initRouterV1(app)
 
