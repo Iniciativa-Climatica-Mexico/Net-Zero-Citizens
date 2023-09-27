2cccd70e61e49c851930e7233cdef1d272d60c74
Daniel Hurtado <a01707774@tec.mx>
2023-09-26 22:08:57 -0600

    fix(multer): add multer middleware

--- backend/src/middlewares/multer.middleware.ts
+++ backend/src/middlewares/multer.middleware.ts
@@ -1,27 +1,22 @@
-// import multer from 'multer';
-// import multerS3 from 'multer-s3';
-// import { s3 } from '../configs/aws.config';
+import multer from 'multer'
+import multerS3 from 'multer-s3'
+import { s3, s3Client } from '../configs/aws.config'
+import { Request } from 'express'
 
-// const upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     bucket: process.env.AWS_BUCKET_NAME || '',
-//     metadata: (req, file, cb) => {
-//       cb(null, { fieldName: file.fieldname });
-//     },
-//     key: (req, file, cb) => {
-//       const { companyId, fileDescription } = req.body;
-//       cb(null, `${companyId}/${file.originalname}/${fileDescription}`);
-//     },
-//     acl: 'public-read'
-//   })
-// });
-
-// export { upload };
-
-
-
-
+export const uploadToS3 = multer({
+  storage: multerS3({
+    s3: s3Client,
+    bucket: process.env.AWS_BUCKET_NAME || '',
+    metadata: (req: Request, file, cb) => {
+      cb(null, { fieldName: file.fieldname })
+    },
+    key: (req: Request, file, cb) => {
+      const { companyId, fileDescription } = req.body
+      cb(null, `${companyId}/${file.originalname}/${fileDescription}`)
+    },
+    acl: 'public-read',
+  }),
+})
 
 // import multer, { StorageEngine, FileFilterCallback } from 'multer'
 // import path from 'path'
