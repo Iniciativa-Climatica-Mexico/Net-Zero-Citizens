cd839774664fc86ecd61134419d86ce639ea2258
Daniel Hurtado <a01707774@tec.mx>
2023-09-27 00:04:18 -0600

    feat(companyFiles): componente para subor archivos

--- ios/GreenCircle/GreenCircle/Presentation/Views/Login/CompanyFilesView.swift
+++ ios/GreenCircle/GreenCircle/Presentation/Views/Login/CompanyFilesView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct CompanyFilesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            VStack(alignment: .leading, spacing: 10){
+                Text("Subir Documentos")
+                    .bold()
+                    .padding(.horizontal, 30)
+                    .font(.system(size: 32))
+                    .padding(.bottom, 22)
+                Text("Para poder validar  tu empresa, te pedimos puedas compartir con nosotros los siguientes documentos")
+                    .padding(.horizontal, 30)
+            }
+            VStack(){
+                
+            }
+        }
     }
 }
 
