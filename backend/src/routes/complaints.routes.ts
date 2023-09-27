980f39184ced9cd3d93545981b67a5539faeab91
unknown <a01709522@tec.mx>
2023-09-27 01:06:58 -0600

    chore(backend): Update complaints routes

--- backend/src/routes/complaints.routes.ts
+++ backend/src/routes/complaints.routes.ts
@@ -5,5 +5,7 @@ const router = express.Router()
 
 router.get('/', controller.getAllComplaints)
 router.get('/:complaintId', controller.getComplaintById)
+router.get('/company/:companyId', controller.getComplaintByCompany)
+router.get('/user/:userId', controller.getComplaintByUser)
 
 export default router
