60ac9ad0bcc18f0a4c6420c7f5ba1ea1025d22ab
salgue441 <carlossalguero441@gmail.com>
2023-09-27 22:33:14 -0600

    feat(BackendConnection): implemented backend connection

--- Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/fragments/company/ComplaintCompanyFragment.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/framework/views/fragments/company/ComplaintCompanyFragment.kt
@@ -4,7 +4,6 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
