cf35fd88e938aced43a8f4fcb1a6aa9894f1eb97
salgue441 <carlossalguero441@gmail.com>
2023-09-28 01:22:47 -0600

    chore(API): removed unused imports

--- Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/complaints/ComplaintAPI.kt
+++ Android/GreenCircle/app/src/main/java/com/greencircle/data/remote/complaints/ComplaintAPI.kt
@@ -1,12 +1,10 @@
 package com.greencircle.data.remote.complaints
 
 import com.greencircle.domain.model.complaints.Complaint
-import java.util.UUID
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.Path
 
 interface ComplaintAPI {
     /**
