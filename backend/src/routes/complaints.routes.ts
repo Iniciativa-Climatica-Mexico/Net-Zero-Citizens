8cad82bad215cad8bd93b471efcbd242e4353d3f
unknown <a01709522@tec.mx>
2023-09-27 22:45:30 -0600

    chore(backend): Update complaints routes

--- backend/src/routes/complaints.routes.ts
+++ backend/src/routes/complaints.routes.ts
@@ -5,9 +5,9 @@ const router = express.Router()
 
 router.get('/', controller.getAllComplaints)
 router.get('/:complaintId', controller.getComplaintById)
-router.get('/company/:companyId', controller.getComplaintByCompany)
+router.get('/company/:companyId', controller.getComplaintsByCompany)
 router.get('/user/:userId', controller.getComplaintByUser)
-router.post('/:userId/:companyId', controller.addComplaint)
+router.post('/create', controller.addComplaint)
 router.put('/:complaintId', controller.updateComplaintStatus)
 
 export default router
