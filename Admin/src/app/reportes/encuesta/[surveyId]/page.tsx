33d73ffb2937f25f06f6d0388a48660f34133714
salgue441 <carlossalguero441@gmail.com>
2023-09-29 15:42:01 -0600

    chore(admin): fix linter error

--- Admin/src/app/reportes/encuesta/[surveyId]/page.tsx
+++ Admin/src/app/reportes/encuesta/[surveyId]/page.tsx
@@ -2,9 +2,7 @@
 
 import { SurveyReport, fetchSurveyReport } from '@/api/v1/report'
 import { QuestionChartContainer } from './components/QuestionChartContainer'
-import { useEffect, useState, useRef } from 'react'
-import { PDFDownloadLink } from '@react-pdf/renderer'
-import SurveyReportPDF from '@/components/SurveyReportPDF'
+import { useEffect, useState } from 'react'
 
 type SurveyReportProps = {
   params: {
@@ -13,9 +11,6 @@ type SurveyReportProps = {
 }
 
 export default function SurveyReportComponent(props: SurveyReportProps) {
-  const chartRef = useRef(null)
-  const [capturedImage, setCapturedImage] = useState<string | null>(null)
-
   try {
     const surveyId = props.params.surveyId
     // const surveyReport = await fetchSurveyReport(surveyId)
@@ -33,19 +28,6 @@ export default function SurveyReportComponent(props: SurveyReportProps) {
     return (
       <div>
         <QuestionChartContainer {...surveyReport} />
-        <div className="mt-4">
-          <PDFDownloadLink
-            document={
-              <SurveyReportPDF
-                surveyReport={surveyReport}
-                chartImage={capturedImage}
-              />
-            }
-            fileName="surveyReport.pdf"
-          >
-            {({ loading }) => (loading ? 'Preparing PDF...' : 'Download PDF')}
-          </PDFDownloadLink>
-        </div>
       </div>
     )
   } catch (e: unknown) {
