95dedc6986373429c2e36014bc991b1e4da29389
Olimpia Garcia <olimpia.gh24@gmail.com>
2023-09-29 10:40:23 -0600

    style(eslint): fix linter problems

--- Admin/src/api/v1/report/index.ts
+++ Admin/src/api/v1/report/index.ts
@@ -30,22 +30,3 @@ export async function fetchSurveyReport(
 
   return res.data as SurveyReport
 }
-
-export async function jsonToCsv(items: Record<string, any>[]): Promise<string> 
-{
-  const header = Object.keys(items[0]);
-  const headerString = header.join(',');
-
-  // handle null or undefined values here
-  const replacer = (key: string, value: any) => (value !== null && value !== undefined ? value : '');
-
-  const rowItems = items.map((row) =>
-    header
-      .map((fieldName) => JSON.stringify(row[fieldName], replacer))
-      .join(',')
-  );
-  // join header and body, and break into separate lines
-  const csv = [headerString, ...rowItems].join('\r\n');
-
-  return csv;
-}
