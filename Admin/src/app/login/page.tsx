3cdb284501c4c0ca0664ce0070dd833e487c86fa
Daniel Cajas <danielsebascm@hotmail.com>
2023-10-03 10:23:09 -0600

    feat(Auth): Styled views

--- Admin/src/app/login/page.tsx
+++ Admin/src/app/login/page.tsx
@@ -25,10 +25,8 @@ export default function Login() {
           </h1>
           <p className="text-sm font-normal">Nos da gusto verte de regreso</p>
         </header>
-        <div className="mt-5">{!isLogged && <GoogleButton />}</div>
-        <input type="text" id='username' />
-        <input type="password" id='password' />
         <div className="mt-5">{!isLogged && <CredentialsButton />}</div>
+        <div className="mt-5">{!isLogged && <GoogleButton />}</div>
       </figure>
       <a href="#" className="text-gray-500">
         Aviso de Privacidad
--- Admin/src/components/CredentialsButton.tsx
+++ Admin/src/components/CredentialsButton.tsx
@@ -2,6 +2,7 @@
 import { signIn } from 'next-auth/react'
 import { useState } from 'react'
 import Modal from 'react-modal'
+import CloseIcon from '@mui/icons-material/Close'
 
 export const CredentialsButton = () => {
   const [modalIsOpen, setIsOpen] = useState(false)
@@ -11,7 +12,7 @@ export const CredentialsButton = () => {
         className="flex w-full justify-center gap-5 rounded bg-white px-4 text-sm font-bold drop-shadow-md hover:bg-gray-50 h-12 items-center rounded-4xl"
         onClick={() => setIsOpen(true)}
       >
-        <span>Continuar con Credenciales</span>
+        <span>Iniciar Sesión</span>
       </button>
 
       <Modal
@@ -30,32 +31,56 @@ export const CredentialsButton = () => {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            minWidth: 'fit-content',
-            minHeight: 'fit-content',
             maxWidth: '40rem',
+            height: 'fit-content',
+            border: 'transparent',
+            background: 'transparent',
           },
         }}
       >
-        <input type="text" placeholder="Email" id="email" />
-        <input type="password" placeholder="Password" id="passwordIn" />
-
-        <button
-          onClick={() => {
-            const email = document.getElementById('email') as HTMLInputElement
-            const password = document.getElementById('passwordIn') as HTMLInputElement
-            console.log(
-              email, password
-            )
-            const cred = {
-              username: email.value,
-              password: password.value,
-            }
-            console.log(cred)
-            signIn('credentials', cred)
-          }}
-        >
-          Iniciar Secion
-        </button>
+        <div className="flex flex-col relative">
+          <div className="flex justify-end w-50 pr-4 pb-2 self-end absolute top-4">
+            <CloseIcon
+              color="disabled"
+              className="cursor-pointer"
+              onClick={() => {
+                setIsOpen(false)
+              }}
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center rounded-lg bg-background border-[2px] border-gray-300 py-16 px-10 shadow-md">
+            <input
+              type="text"
+              className="px-2 py-2 mb-3 rounded border border-solid border-gray-300 w-full h-11 min-w-xl focus:border-primary-700"
+              placeholder="Email"
+              id="email"
+            />
+            <input
+              type="password"
+              className="px-2 py-2 mb-3 rounded border border-gray-300 w-full h-11 focus:border-primary-700"
+              placeholder="Password"
+              id="passwordIn"
+            />
+            <button
+              className=" bg-primary-base hover:bg-primary-900 text-white font-bold py-2 px-4 rounded mt-4 shadow-md"
+              onClick={() => {
+                const email = document.getElementById(
+                  'email'
+                ) as HTMLInputElement
+                const password = document.getElementById(
+                  'passwordIn'
+                ) as HTMLInputElement
+                const cred = {
+                  username: email.value,
+                  password: password.value,
+                }
+                signIn('credentials', cred)
+              }}
+            >
+              Iniciar Sesión
+            </button>
+          </div>
+        </div>
       </Modal>
     </>
   )
