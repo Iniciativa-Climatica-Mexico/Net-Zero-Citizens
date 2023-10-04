09c8f0d24329c8f7188a3bc009ddbd87873d7f3c
David Langarica <david.langarica@gmail.com>
2023-10-03 22:20:22 -0600

    feat(deleteUserRoute): Add users route

--- Admin/src/components/navbar.tsx
+++ Admin/src/components/navbar.tsx
@@ -67,7 +67,7 @@ export default function Navbar() {
                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     aria-current="page"
                   >
-                    Proovedores
+                    Proveedores
                   </a>
                 </li>
                 <li>
@@ -80,7 +80,7 @@ export default function Navbar() {
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href="/usuarios"
                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
                     Usuarios
