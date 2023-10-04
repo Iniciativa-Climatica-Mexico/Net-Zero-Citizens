914a74b8680a8e445041fa6f82b0b21a02b5439a
David Langarica <david.langarica@gmail.com>
2023-10-04 00:06:18 -0600

    feat(deleteUsers): delete users working

--- Admin/src/app/usuarios/page.tsx
+++ Admin/src/app/usuarios/page.tsx
@@ -1,11 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import {
-  Company,
-  getPendingCompanies,
-  getApprovedCompanies,
-} from '@/api/v1/company'
+import { useEffect, useState } from 'react'
 
 import { Avatar, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
@@ -20,122 +15,75 @@ import {
   TableRow,
 } from '@/components/ui/table'
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import LogoSm from './../../../public/LogoSm.svg'
 
-import { CellAction } from '@/components/cellAction'
-import ModalProveedor from '@/components/modalProveedor'
+import { User } from '@/@types/users/users'
+import { getAllUsers } from '@/api/v1/users'
 import Image from 'next/image'
+import DeleteUsersButton from '@/components/users/DeleteUsersButton'
+import DeleteUserConfirmationModal from '@/components/users/DeleteUserConfirmationModal'
 
-export type tabs = 'pending_approval' | 'approved' | 'no_user'
-
-export default function Home() {
-  const [selectedCompany, setSelectedCompany] = useState<Company>({
-    companyId: '',
-    userId: null,
-    name: '',
-    profilePicture: '',
-    state: '',
-    city: '',
-    street: '',
-    zipCode: '',
-    status: 'pending_approval',
-    email: '',
-    phone: '',
-    webPage: '',
-    description: '',
-    createdAt: '',
-    streetNumber: '',
-    pdfCurriculumUrl: '',
-    pdfDicCdmxUrl: '',
-    pdfPeeFideUrl: '',
-    pdfGuaranteeSecurityUrl: '',
-    pdfActaConstitutivaUrl: '',
-    pdfIneUrl: '',
-  })
-  const [modalOpen, setIsModalOpen] = useState(false)
+export default function Users() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const [currentPage, setCurrentPage] = useState(1)
   const itemsPerPage = 8
-  const [pendingCompanies, setPendingCompanies] = useState<Company[]>([])
-  const [approvedCompanies, setApprovedCompanies] = useState<Company[]>([])
+  const [users, setUsers] = useState<User[]>([])
   const [searchTerm, setSearchTerm] = useState('')
-  const [activeTab, setActiveTab] = useState<tabs>('pending_approval')
+  const [selectedUser, setSelectedUser] = useState<User|null>(null)
 
-  const handleTableRowClick = (company: Company) => {
-    setSelectedCompany(company)
-    setIsModalOpen(true)
-  }
-
-  const fetchPendingCompanies = async () => {
+  const fetchUsers = async () => {
     try {
-      const companies = await getPendingCompanies()
-      setPendingCompanies(companies)
+      const users = await getAllUsers()
+      setUsers(users)
     } catch (error) {
-      console.error('Fetch of pending companies was not successful', error)
+      console.error('Fetch of users was not successful', error)
     }
   }
 
-  const fetchApprovedCompanies = async () => {
-    try {
-      const companies = await getApprovedCompanies()
-      setApprovedCompanies(companies)
-    } catch (error) {
-      console.error('Fetch of approved companies was not successful', error)
-    }
-  }
-
-  const filteredCompanies =
-    activeTab === 'pending_approval'
-      ? pendingCompanies?.filter((company) =>
-        company.name.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-      : activeTab === 'approved'
-        ? approvedCompanies?.filter((company) =>
-          company.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-        : approvedCompanies?.filter(
-          (company) =>
-            company.userId === null &&
-            company.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
+  const filteredUsersBySearchTerm = users?.filter((user) => {
+    const fullName = `${user.firstName} ${user.lastName}`
+    const email = user.email
+    const includesFullName = fullName.toLowerCase().includes(searchTerm.toLowerCase())
+    const includesEmail = email.toLowerCase().includes(searchTerm.toLowerCase())
+    return includesFullName || includesEmail
+  })
 
   const startIndex = (currentPage - 1) * itemsPerPage
   const endIndex = startIndex + itemsPerPage
-  const paginatedCompanies = filteredCompanies?.slice(startIndex, endIndex)
+  const paginatedUsers = filteredUsersBySearchTerm?.slice(startIndex, endIndex)
 
   const handlePageChange = (newPage: number) => setCurrentPage(newPage)
 
+  const handleOnDeleteUserClick = (user: User) => {
+    setSelectedUser(user)
+    setIsModalOpen(true)
+  }
+
   useEffect(() => {
-    fetchPendingCompanies()
-    fetchApprovedCompanies()
+    fetchUsers()
   }, [])
 
-  const renderTable = (companies: Company[]) => (
+  const renderTable = (users: User[]) => (
     <Table className="border border-[#C1C9D2] rounded">
       <TableCaption></TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead className="w-[100px]">Imagen</TableHead>
-          {activeTab === 'no_user' ? (
-            <TableHead>Token de registro</TableHead>
-          ) : null}
-          <TableHead>Nombre</TableHead>
+          <TableHead>Nombre(s)</TableHead>
+          <TableHead>Apellido(s)</TableHead>
           <TableHead>Correo</TableHead>
-          <TableHead>Ubicación</TableHead>
-          <TableHead>Estado</TableHead>
           <TableHead className="text-right"></TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {companies?.map((company) => (
-          <TableRow key={company.companyId}>
+        {users?.map((user) => (
+          <TableRow key={user.userId}>
             <TableCell
               className="cursor-pointer"
-              onClick={() => handleTableRowClick(company)}
             >
-              {company.profilePicture != null ? (
+              {user.profilePicture != null ? (
                 <Avatar>
-                  <AvatarImage src={company.profilePicture} />
+                  <AvatarImage src={user.profilePicture} />
                 </Avatar>
               ) : (
                 <Image
@@ -147,55 +95,27 @@ export default function Home() {
                 />
               )}
             </TableCell>
-            {activeTab === 'no_user' ? (
-              <TableCell
-                className="cursor-pointer"
-                onClick={() => handleTableRowClick(company)}
-              >
-                {company.companyId}
-              </TableCell>
-            ) : null}
             <TableCell
               className="cursor-pointer"
-              onClick={() => handleTableRowClick(company)}
             >
-              {company.name}
+              {user.firstName}
             </TableCell>
             <TableCell
               className="cursor-pointer"
-              onClick={() => handleTableRowClick(company)}
             >
-              {company.email}
+              {user.lastName}
             </TableCell>
             <TableCell
               className="cursor-pointer"
-              onClick={() => handleTableRowClick(company)}
             >
-              {`${company.street} ${company.city}, ${company.state} ${company.zipCode}`}
+              {user.email}
             </TableCell>
-            <TableCell
-              className="cursor-pointer"
-              onClick={() => handleTableRowClick(company)}
+            <TableCell 
+              className="text-right"
+              onClick={() => handleOnDeleteUserClick(user)}
             >
-              <div
-                className={`${
-                  company.status === 'approved'
-                    ? 'bg-[#547C8B] text-white'
-                    : 'bg-[#FFE6C2] text-jet'
-                }
-                text-center rounded-xl p-2.5`}
-              >
-                {company.status === 'approved' ? 'Aprobado' : 'Pendiente'}
-              </div>
-            </TableCell>
-            <TableCell className="text-right">
-              <CellAction
+              <DeleteUsersButton
                 setIsModalOpen={setIsModalOpen}
-                companyId={company.companyId}
-                fetchPendingCompanies={() => fetchPendingCompanies()}
-                fetchApprovedCompanies={() => fetchApprovedCompanies()}
-                company={company}
-                activeTab={activeTab}
               />
             </TableCell>
           </TableRow>
@@ -206,55 +126,26 @@ export default function Home() {
 
   return (
     <>
-      {modalOpen && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black opacity-60 z-20"></div>
-      )}
-      {modalOpen && (
-        <div className="flex flex-col items-center justify-center h-screen absolute left-1/2 right-1/2 z-30">
-          <ModalProveedor
-            selectedCompany={selectedCompany}
+      {isModalOpen && 
+          <DeleteUserConfirmationModal
+            userId={selectedUser?.userId}
+            firstName={selectedUser?.firstName}
+            lastName={selectedUser?.lastName}
             setIsModalOpen={setIsModalOpen}
-            fetchPendingCompanies={() => fetchPendingCompanies()}
-            fetchApprovedCompanies={() => fetchApprovedCompanies()}
-            activeTab={activeTab}
+            fetchUsers={fetchUsers}
           />
-        </div>
-      )}
+      }
       <main className="border border-[#C1C9D2] m-[30px] mt-[15px] p-[20px] pb-5 rounded-lg">
-        <h1 className="text-[20px] font-bold">Descubre Proveedores</h1>
+        <h1 className="text-[20px] font-bold">Usuarios</h1>
         <div className="flex items-center py-4 gap-x-2">
           <Input
-            placeholder="Busca un proveedor"
+            placeholder="Busca un usuario"
             className="max-w-sm"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </div>
-        <Tabs defaultValue="pending_approval">
-          <TabsList>
-            <TabsTrigger
-              value="pending_approval"
-              onClick={() => setActiveTab('pending_approval')}
-            >
-              Pendientes
-            </TabsTrigger>
-            <TabsTrigger
-              value="approved"
-              onClick={() => setActiveTab('approved')}
-            >
-              Aprobados
-            </TabsTrigger>
-            <TabsTrigger
-              value="no_user"
-              onClick={() => setActiveTab('no_user')}
-            >
-              Sin Responsable
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value={activeTab}>
-            {renderTable(paginatedCompanies)}
-          </TabsContent>
-        </Tabs>
+        {renderTable(paginatedUsers)}
         <div className="flex justify-end items-center pt-2 gap-x-2">
           <Button
             variant="outline"
@@ -267,7 +158,7 @@ export default function Home() {
           <Button
             variant="outline"
             onClick={() => handlePageChange(currentPage + 1)}
-            disabled={endIndex >= filteredCompanies?.length}
+            disabled={endIndex >= filteredUsersBySearchTerm?.length}
           >
             Siguiente
           </Button>
