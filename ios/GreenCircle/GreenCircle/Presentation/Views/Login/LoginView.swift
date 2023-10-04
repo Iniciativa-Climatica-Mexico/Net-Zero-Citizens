e5da32466783d0936e824abe816613909b4995a2
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-10-04 12:14:46 -0600

    fix(sizes): UI now looks good on all devices

--- ios/GreenCircle/GreenCircle/Presentation/Views/Login/LoginView.swift
+++ ios/GreenCircle/GreenCircle/Presentation/Views/Login/LoginView.swift
@@ -1,5 +1,5 @@
 //
-//  LoginView.swift
+//  k.swift
 //  GreenCircle
 //
 //  Created by Ricardo Adolfo Fernández Alvarado on 12/09/23.
@@ -17,83 +17,84 @@ struct LoginView: View {
   @StateObject var viewModel = LoginViewModel()
   
   var body: some View {
-    
-    VStack {
-      HeaderView(
-        title: "Inicia sesión con tu cuenta",
-        subTitle: "Nos da gusto verte")
-      .padding(.bottom)
-      Spacer()
-      
+    ScrollView {
       VStack {
+        HeaderView(
+          title: "Inicia sesión con tu cuenta",
+          subTitle: "Nos da gusto verte")
+        .padding(.bottom)
+        Spacer()
         
-        VStack(spacing: 20) {
-          InputFormView(bindingValue: $viewModel.user,
-                        label: "Usuario",
-                        prompt: "Usuario")
-          .keyboardType(.emailAddress)
-          .autocorrectionDisabled()
-          .textInputAutocapitalization(.never)
-          SecureInputFormView(bindingValue: $viewModel.password,
-                              label: "Contraseña",
-                              prompt: "Contraseña")
-          .padding(.bottom)
-          MainButton("Iniciar Sesión") {
+        VStack {
+          
+          VStack(spacing: 20) {
+            InputFormView(bindingValue: $viewModel.user,
+                          label: "Usuario",
+                          prompt: "Usuario")
+            .keyboardType(.emailAddress)
+            .autocorrectionDisabled()
+            .textInputAutocapitalization(.never)
+            SecureInputFormView(bindingValue: $viewModel.password,
+                                label: "Contraseña",
+                                prompt: "Contraseña")
+            .padding(.bottom)
+            MainButton("Iniciar Sesión") {
+              Task {
+                let res = await viewModel.handleSignIn()
+                
+                switch res {
+                case .success:
+                  goMainMenu()
+                default:
+                  break
+                }
+              }
+            }
+          }.padding(.bottom)
+          
+          ButtonDividerView(text: "O continúa con")
+          
+          GoogleSignInButton(style: .wide) {
             Task {
-              let res = await viewModel.handleSignIn()
-              
-              switch res {
+              let state = await viewModel
+                .handleGoogleSignIn()
+              switch state {
+              case .fail:
+                break
+              case .newUser:
+                goForm()
               case .success:
                 goMainMenu()
-              default:
-                break
               }
             }
           }
-        }.padding(.bottom)
-        
-        ButtonDividerView(text: "O continúa con")
-        
-        GoogleSignInButton(style: .wide) {
-          Task {
-            let state = await viewModel
-              .handleGoogleSignIn()
-            switch state {
-            case .fail:
-              break
-            case .newUser:
-              goForm()
-            case .success:
-              goMainMenu()
-            }
+          .alert("Algo salió mal",
+                 isPresented: $viewModel.showAlert) {
+            Button("Entendido", role: .cancel) {}
+          } message: {
+            Text(viewModel.alertMessage)
           }
         }
-        .alert("Algo salió mal",
-               isPresented: $viewModel.showAlert) {
-          Button("Entendido", role: .cancel) {}
-        } message: {
-          Text(viewModel.alertMessage)
+        .padding(.horizontal)
+        
+        Divider().padding()
+        
+        HStack {
+          Text("¿No tienes una cuenta?")
+          Spacer()
+          LinkButton("Regístrate", buttonColor: .blue){
+            goUserRegister()
+          }
         }
+        .padding()
+        
+        LinkButton("Soy Proveedor", buttonColor: .blue, action: goCompanyRegister)
+          .padding(.bottom, 8)
       }
-      .padding(.horizontal)
-      
-      Divider().padding()
-      
-      HStack {
-        Text("¿No tienes una cuenta?")
-        Spacer()
-        LinkButton("Regístrate", buttonColor: .blue){
-          goUserRegister()
-        }
+      .foregroundColor(Color("MainText"))
+      .onTapGesture {
+        hideKeyboard()
       }
-      .padding()
-      
-      LinkButton("Soy Proveedor", buttonColor: .blue, action: goCompanyRegister)
-        .padding(.bottom, 8)
-    }
-    .foregroundColor(Color("MainText"))
-    .onTapGesture {
-      hideKeyboard()
     }
   }
 }
