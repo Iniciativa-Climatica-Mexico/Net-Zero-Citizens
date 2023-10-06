68cda6abb04a3ed9d165b09c2c943f4888679318
unknown <a01709522@tec.mx>
2023-10-05 18:27:58 -0600

    chore(backend): Fix complaint tests

--- backend/test/complaint.spec.ts
+++ backend/test/complaint.spec.ts
@@ -38,6 +38,36 @@ const testComplaints = [
     user: { firstName: 'John', lastName: 'Doe' },
     userId: '8de45630-2e76-4d97-98c2-9ec0d1f3a5b8',
   },
+  {
+    complaintId: 'a2c0e7e0-4b1a-4e1a-9f1a-0e5a7a1b0e7e',
+    userId: '8de45630-2e76-4d97-98c2-9ec0d1f3a5b8',
+    companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
+    complaintSubject: 'Comportamiento Inapropiado',
+    complaintDescription: 'El vendedor me insultó',
+    complaintStatus: 'active',
+    user: { firstName: 'John', lastName: 'Doe' },
+    company: { name: 'SUNPOWER' }
+  },
+  {
+    complaintId: 'b2c0e7e0-4b1a-4e1a-9f1a-0e5a9a1b0e7e',
+    userId: '8de45630-2e76-4d97-98c2-9ec0d1f3a5b8',
+    companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
+    complaintSubject: 'Comportamiento Inapropiado',
+    complaintDescription: 'El vendedor me insultó',
+    complaintStatus: 'active',
+    user: { firstName: 'John', lastName: 'Doe' },
+    company: { name: 'SUNPOWER' }
+  },
+  {
+    complaintId: 'b250e7e0-4b1a-4e1a-9f1a-0e5a9a1b0e7e',
+    userId: '8de45630-2e76-4d97-98c2-9ec0d1f3a5b8',
+    companyId: 'c1b0e7e0-0b1a-4e1a-9f1a-0e5a9a1b0e7e',
+    complaintSubject: 'Comportamiento Inapropiado',
+    complaintDescription: 'El vendedor me insultóoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo',
+    complaintStatus: 'active',
+    user: { firstName: 'John', lastName: 'Doe' },
+    company: { name: 'SUNPOWER' }
+  }
 ]
 
 const attributesToExclude = ['createdAt', 'updatedAt']
