0e9e749cca8b80e5df285845ae40fd1972156d8a
FloresTM <seb.flores2002@gmail.com>
2023-10-08 19:36:53 -0600

    fix(ios): Fixed conflict

--- ios/GreenCircle/GreenCircle/Presentation/Views/CoordinatesView.swift
+++ ios/GreenCircle/GreenCircle/Presentation/Views/CoordinatesView.swift
@@ -1,4 +1,3 @@
-
 import SwiftUI
 import MapKit
 import Combine
@@ -6,9 +5,11 @@ import Combine
 class IdentifiablePointAnnotation: NSObject, MKAnnotation, Identifiable {
     let id = UUID()
     let wrappedAnnotation: MKPointAnnotation
+    let name: String
 
-    init(annotation: MKPointAnnotation) {
+    init(annotation: MKPointAnnotation, name: String) {
         self.wrappedAnnotation = annotation
+        self.name = name
     }
 
     var coordinate: CLLocationCoordinate2D {
@@ -24,6 +25,7 @@ struct CoordinatesView: View {
     @ObservedObject var viewModel = CoordinatesCompanyViewModel()
     @State var region = MKCoordinateRegion()
     @State var annotations: [IdentifiablePointAnnotation] = []
+    @State var selectedAnnotation: IdentifiablePointAnnotation?
     
     @State private var userLocation: CLLocationCoordinate2D?
     @State private var isTrackingUserLocation = true
@@ -31,7 +33,28 @@ struct CoordinatesView: View {
     
     var body: some View {
         Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(isTrackingUserLocation ? .follow : .none), annotationItems: annotations) { annotation in
-            MapMarker(coordinate: annotation.coordinate)
+            MapAnnotation(coordinate: annotation.coordinate) {
+                VStack {
+                    if annotation.id == selectedAnnotation?.id {
+                        Text(annotation.name)
+                            .font(.headline)
+                            .padding(4)
+                            .background(Color.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 4))
+                            .shadow(radius: 4)
+                            .fixedSize(horizontal: true, vertical: true)
+                            .frame(maxWidth: 200)
+                    }
+                    Image(systemName: "mappin")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 30)
+                        .foregroundColor(.red)
+                        .onTapGesture {
+                            selectedAnnotation = annotation
+                        }
+                }
+            }
         }
 
         .onAppear {
@@ -47,14 +70,12 @@ struct CoordinatesView: View {
     func setRegion() async {
         await viewModel.getCoordinates()
         let coordinates = viewModel.coordinates
-        
 
         annotations = coordinates.map { company -> IdentifiablePointAnnotation in
             let annotation = MKPointAnnotation()
             annotation.coordinate = CLLocationCoordinate2D(latitude: company.latitude, longitude: company.longitude)
-            return IdentifiablePointAnnotation(annotation: annotation)
+            return IdentifiablePointAnnotation(annotation: annotation, name: company.name)
         }
-        
 
         let coordinatesRegion = MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: coordinates.first?.latitude ?? 0, longitude: coordinates.first?.longitude ?? 0),
@@ -73,7 +94,7 @@ struct CoordinatesView: View {
                 }
             } receiveValue: { coordinates in
                 self.coordinates = (coordinates.latitude, coordinates.longitude)
-                self.userLocation = coordinates 
+                self.userLocation = coordinates
             }
             .store(in: &tokens)
     }
@@ -92,4 +113,3 @@ struct CoordinatesView: View {
         }
     }
 }
-
