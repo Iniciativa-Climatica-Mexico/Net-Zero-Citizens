be9c118673b5f41afc14bfb24fcc49ba387b2e78
danfupo03 <danfupo@outlook.com>
2023-10-10 17:33:07 -0600

    feat(iOSFilters): Add searchbar and filter button, also create a component for company card

---
+++ ios/GreenCircle/GreenCircle/Presentation/Views/Components/FilterView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterView.swift
+//  GreenCircle
+//
+//  Created by Dan FuPo on 10/10/23.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterView()
+}
