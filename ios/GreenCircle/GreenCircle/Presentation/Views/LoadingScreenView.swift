0dda9231ed10c666f2b892aa1ce07ef589e03cce
DonCake <71788652+DonCake@users.noreply.github.com>
2023-10-12 08:59:17 -0600

    feat(ios): Cambio de estado de loader despues de ciertos segundos

--- ios/GreenCircle/GreenCircle/Presentation/Views/LoadingScreenView.swift
+++ ios/GreenCircle/GreenCircle/Presentation/Views/LoadingScreenView.swift
@@ -8,9 +8,8 @@
 import SwiftUI
 
 struct LoadingScreenView: View {
-    @State var isAnimating = false
     @State private var rotationDegrees = 0.0
-
+    @StateObject var viewModel = LoadingScreenViewModel()
     private var animation: Animation {
         .linear
         .speed(0.1)
@@ -21,25 +20,32 @@ struct LoadingScreenView: View {
           Color(.systemBackground)
               .ignoresSafeArea()
               .opacity(0.7)
-          VStack {
+         
               Image("GCLOGO")
                   .resizable()
                   .aspectRatio(contentMode: .fit)
                   .frame(height: 50)
-                  .opacity(0.5)
+                  .opacity(viewModel.loader1 ? 0.5 : 0.00)
                   .rotationEffect(.degrees(rotationDegrees))
                 
                   .onAppear {
-                      withAnimation(animation) {
+                      withAnimation(Animation.easeIn(duration: 5.00)) {
                           rotationDegrees = 360.0
                       }
 
                   }
+              Image("GCLOGOGRAY")
+                  .resizable()
+                  .aspectRatio(contentMode: .fit)
+                  .frame(height: 50)
+                  .opacity(viewModel.loader2 ? 0.4 : 0.00)
+                  
+                  
               
-            }
+            
                   
         }
-         
+      .onAppear{viewModel.hideLoaderAfterTime()}
     }
     
 }
