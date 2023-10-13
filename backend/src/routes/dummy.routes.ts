71bd0c652f8534cf3445b36fc6d4ff95cde20bc5
Daniel Cajas <danielsebascm@hotmail.com>
2023-10-13 14:04:04 -0600

    fix(Dummy): kill

--- backend/src/routes/dummy.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import * as controller from '../controllers/dummy.controller'
-
-const router = express.Router()
-
-router.get('/', controller.getAllDummys)
-router.get('/:name', controller.getGreeting)
-
-export default router
