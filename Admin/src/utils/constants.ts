fcce12d289a8bac4df80f08913cc160179c9757c
Daniel Cajas <danielsebascm@hotmail.com>
2023-10-14 00:15:39 -0600

    fix(Ecoinfo): Fetch

--- Admin/src/utils/constants.ts
+++ Admin/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_BASE_URL = 'https://api.iniciativaclimatica.org'
+export const SERVER_BASE_URL = 'https://api.iniciativaclimatica.org/api/v1'
 export const AUTH_TOKEN_KEY = 'GC_AUTH_TOKEN'
 export const REFRESH_TOKEN_KEY = 'GC_REFRESH_TOKEN'
 export const USER_KEY = 'GC_USER'
