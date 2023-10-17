a604953ce7308d607deb679c55a8fbc15722d744
David Langarica <david.langarica@gmail.com>
2023-10-17 17:38:42 -0600

    style(fixes): repsonsive fixes

--- Landing/src/sections/PrimaryInfo.tsx
+++ Landing/src/sections/PrimaryInfo.tsx
@@ -15,23 +15,31 @@ export default function PrimaryInfo() {
   return (
     <section className="bg-primary-blue px-8 py-32 w-full text-center text-primary-white lg:px-40">
       <article className="flex flex-col gap-28 lg:flex-row lg:gap-44">
-        <div className="flex flex-col items-center gap-5">
+        <div className="flex flex-col items-center gap-5 w-full">
           <StarIcon />
-          <h2 className="text-3xl font-semibold">¿Qué puedes hacer?</h2>
+          <h2 className="text-3xl font-semibold">
+            Protege el clima y tus finanzas
+          </h2>
           <p>
-            GreenCircle es una aplicación donde conectamos de manera efectiva a
-            proveedores comprometidos con la sostenibilidad ambiental con
-            consumidores conscientes, que desean tomar decisiones de compra más
-            responsables.
+            Optar por energía solar, tanto para la electricidad como para
+            calentar agua, es de las acciones más importantes que puedes tomar
+            en casa para proteger el clima, el medio ambiente y tu futuro. La
+            energía renovable es la manera más inteligente de proteger tu
+            economía ante la ineludible inflación de los precios de la energía.
+          </p>
+          <p>
+            Protege tu futuro financiero y el de tu familia, para que te
+            asegures de siempre tener recursos para mantener tu modo de vida,
+            libre de cualquier dependencia a la falible red eléctrica.
           </p>
         </div>
-        <div className="flex flex-col items-center gap-5">
+        <div className="flex flex-col items-center gap-5 w-full">
           <PeopleIcon />
           <h2 className="text-3xl font-semibold">¿Cómo funciona?</h2>
           <p>
-            A través de GreenCircle los usuarios pueden explorar una amplia
-            variedad de productos y servicios ofrecidos por proveedores que
-            comparten su compromiso con la conservación del medio ambiente.
+            A través de GreenCircle puedes explorar los distintos productos y
+            servicios de energía solar para acercarte a la descarbonización y
+            que ayudes al medio ambiente.
           </p>
         </div>
       </article>
